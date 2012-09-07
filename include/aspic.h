c3117291564a0c41c365e08fc6430a3b5baa1d68
chris <chris@ringeval.com>
2012-09-07 18:39:19 +0200

    adding include file aspic.h

---
+++ include/aspic.h
@@ -0,0 +1,83 @@
+!brute force importation of all modules and all function. Use this
+!include file only if you are lazy enough to not pick up only what you
+!want.
+
+!one parameter models
+use hisr
+use hireheat
+use lfisr
+use lfireheat
+use mlfisr
+use mlfireheat
+use rcmisr
+use rcmireheat
+use rcqisr
+use rcqireheat
+use pnisr
+use pnireheat
+use mnisr
+use mnireheat
+use esisr
+use esireheat
+use plisr
+use plireheat
+use kmiisr
+use kmiireheat
+use hf1isr
+use hf1ireheat
+use cwisr
+use cwireheat
+use lisr
+use lireheat
+use twisr
+use twireheat
+use rpisr
+use rpireheat
+use dwisr
+use dwireheat
+use mhisr
+use mhireheat
+use risr
+use rireheat
+
+!two parameters models
+use sfisr
+use sfireheat
+use iisr
+use iireheat
+use kmiiisr
+use kmiiireheat
+use lmi1sr
+use lmi1reheat
+use lmi2sr
+use lmi2reheat
+use lmi3sr
+use lmi3reheat
+use hf2isr
+use hf2ireheat
+use mssmisr
+use mssmireheat
+use bsusybisr
+use bsusybireheat
+use tisr
+use tireheat
+use beisr
+use beireheat
+use pcnisr
+use pcnireheat
+use nckisr
+use nckireheat
+
+!three parameter models
+use rmisr
+use rmireheat
+use vhisr
+use vhireheat
+use dsisr
+use dsireheat
+use gmisr
+use gmireheat
+use shisr
+use shireheat
+use su5isr
+use su5ireheat
