ff2d2ba0c6c60a265730c0a308e363c17b3b3a32
chris <chris@ringeval.com>
2013-04-25 17:09:55 +0200

    Adding lnR/Rrad for cnci

--- src/cnci/cncimain.f90
+++ src/cnci/cncimain.f90
@@ -8,9 +8,15 @@ program cncimain
   use infinout, only : delete_file, livewrite
   use srreheat, only : log_energy_reheat_ingev
 
+  use cncisr, only : cnci_norm_potential
+  use cncireheat, only : cnci_x_rreh, cnci_x_rrad
+  use srreheat, only : get_lnrrad_rreh, get_lnrreh_rrad, ln_rho_endinf
+  use srreheat, only : get_lnrrad_rhow, get_lnrreh_rhow, ln_rho_reheat
+
+
   implicit none
 
-  
+
   real(kp) :: Pstar, logErehGeV, Treh
 
   integer :: i,j,k
@@ -23,12 +29,15 @@ program cncimain
   real(kp) :: lnRhoRehMin, lnRhoRehMax
   real(kp), dimension(2) :: vecbuffer
 
+  real(kp) :: lnRmin, lnRmax, lnR, lnRhoEnd
+  real(kp) :: lnRradMin, lnRradMax, lnRrad
+  real(kp) :: VendOverVstar, eps1End
 
   Pstar = powerAmpScalar
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-!!          Calculates the prior space and               !!
+  !!          Calculates the prior space and               !!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
@@ -38,16 +47,16 @@ program cncimain
 
   call delete_file('cnci_xendmax.dat')
   do i=1,nalpha
-       alpha=alphamin+(alphamax-alphamin)*(real(i,kp)/real(nalpha,kp))
+     alpha=alphamin+(alphamax-alphamin)*(real(i,kp)/real(nalpha,kp))
 
-       call livewrite('cnci_xendmin.dat',alpha,cnci_xendmin(40._kp,alpha), &
-       cnci_xendmin(60._kp,alpha),cnci_xendmin(80._kp,alpha))
+     call livewrite('cnci_xendmin.dat',alpha,cnci_xendmin(40._kp,alpha), &
+          cnci_xendmin(60._kp,alpha),cnci_xendmin(80._kp,alpha))
   end do
 
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-!!        Calculates the reheating predictions           !!
+  !!        Calculates the reheating predictions           !!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
@@ -56,7 +65,7 @@ program cncimain
 
 
   w=0._kp
-!  w = 1._kp/3._kp
+  !  w = 1._kp/3._kp
 
   call delete_file('cnci_predic.dat')
   call delete_file('cnci_nsr.dat')
@@ -66,25 +75,25 @@ program cncimain
   alphavalues(1)=0.2_kp
 
   do j=1,3
-   alpha=alphavalues(j)
+     alpha=alphavalues(j)
 
 
 
-  !Prior on xend
+     !Prior on xend
      if (alpha .eq. 10._kp**(-3.)) then
-     xendmin=cnci_xendmin(55._kp,alpha)
-     xendmax=30._kp*xendmin
-     nxend=400
+        xendmin=cnci_xendmin(55._kp,alpha)
+        xendmax=30._kp*xendmin
+        nxend=400
      endif
      if (alpha .eq. 10._kp**(-1.)) then
-     xendmin=cnci_xendmin(58._kp,alpha)
-     xendmax=10._kp*xendmin
-     nxend=100
+        xendmin=cnci_xendmin(58._kp,alpha)
+        xendmax=10._kp*xendmin
+        nxend=100
      endif
      if (alpha .eq. 0.2_kp) then
-     xendmin=cnci_xendmin(60._kp,alpha)
-     xendmax=2._kp*xendmin
-     nxend=100
+        xendmin=cnci_xendmin(60._kp,alpha)
+        xendmax=2._kp*xendmin
+        nxend=100
      endif
 
 
@@ -120,14 +129,51 @@ program cncimain
            call livewrite('cnci_predic.dat',alpha,xend,xendmin,eps1,eps2,eps3,r,ns,Treh)
 
            call livewrite('cnci_nsr.dat',ns,r,abs(bfoldstar),lnRhoReh)
-  
+
         end do
 
      end do
 
- end do
+  end do
 
 
+  write(*,*)
+  write(*,*)'Testing Rrad/Rreh'
 
+  lnRradmin=-42
+  lnRradmax = 10
+  alpha = 1e-2
+  xend = 100
+  do i=1,npts
+
+     lnRrad = lnRradMin + (lnRradMax-lnRradMin)*real(i-1,kp)/real(npts-1,kp)
+
+     xstar = cnci_x_rrad(alpha,xend,lnRrad,Pstar,bfoldstar)
+
+     print *,'lnRrad=',lnRrad,' bfoldstar= ',bfoldstar, 'xstar', xstar
+
+     eps1 = cnci_epsilon_one(xstar,alpha)
+
+     !consistency test
+     !get lnR from lnRrad and check that it gives the same xstar
+     eps1end =  cnci_epsilon_one(xend,alpha)
+     VendOverVstar = cnci_norm_potential(xend,alpha)/cnci_norm_potential(xstar,alpha)
+
+     lnRhoEnd = ln_rho_endinf(Pstar,eps1,eps1End,VendOverVstar)
+
+     lnR = get_lnrreh_rrad(lnRrad,lnRhoEnd)
+     xstar = cnci_x_rreh(alpha,xend,lnR,bfoldstar)
+     print *,'lnR',lnR, 'bfoldstar= ',bfoldstar, 'xstar', xstar
+
+     !second consistency check
+     !get rhoreh for chosen w and check that xstar gotten this way is the same
+     w = 0._kp
+     lnRhoReh = ln_rho_reheat(w,Pstar,eps1,eps1End,-bfoldstar,VendOverVstar)
+
+     xstar = cnci_x_star(alpha,xend,w,lnRhoReh,Pstar,bfoldstar)
+     print *,'lnR', get_lnrreh_rhow(lnRhoReh,w,lnRhoEnd),'lnRrad' &
+          ,get_lnrrad_rhow(lnRhoReh,w,lnRhoEnd),'xstar',xstar
+
+  enddo
 
 end program cncimain
--- src/cnci/cncireheat.f90
+++ src/cnci/cncireheat.f90
@@ -5,6 +5,8 @@ module cncireheat
   use inftools, only : zbrent
   use srreheat, only : get_calfconst, find_reheat, slowroll_validity
   use srreheat, only : display, pi, Nzero, ln_rho_endinf,ln_rho_reheat
+  use srreheat, only : find_reheat_rrad, find_reheat_rreh
+  use srreheat, only : get_calfconst_rrad, get_calfconst_rreh
   use cncisr, only : cnci_epsilon_one, cnci_epsilon_two, cnci_epsilon_three
   use cncisr, only : cnci_norm_potential,cnci_efold_primitive,cnci_x_epsoneunity
   implicit none
@@ -12,10 +14,11 @@ module cncireheat
   private
 
   public cnci_x_star, cnci_lnrhoreh_max,find_cnci_x_star
+  public cnci_x_rrad, cnci_x_rreh
 
 contains
 
-!returns x given potential parameters, scalar power, wreh and
+!returns x potential parameters, scalar power, wreh and
 !lnrhoreh. If present, returns the correspoding bfoldstar
   function cnci_x_star(alpha,xend,w,lnRhoReh,Pstar,bfold)    
     implicit none
@@ -74,11 +77,136 @@ contains
     epsOneStar = cnci_epsilon_one(x,alpha)
     potStar = cnci_norm_potential(x,alpha)
 
-
     find_cnci_x_star = find_reheat(PrimStar,calFplusPrimEnd,w,epsOneStar,potStar)
 
-
   end function find_cnci_x_star
+
+
+
+!returns x given potential parameters, scalar power, and lnRrad.
+!If present, returns the corresponding bfoldstar
+  function cnci_x_rrad(alpha,xend,lnRrad,Pstar,bfold)    
+    implicit none
+    real(kp) :: cnci_x_rrad
+    real(kp), intent(in) :: alpha,xEnd,lnRrad,Pstar
+    real(kp), optional :: bfold
+
+    real(kp), parameter :: tolFind=tolkp
+    real(kp) :: mini,maxi,calF,x
+    real(kp) :: primEnd,epsOneEnd,potEnd
+
+    type(transfert) :: cnciData
+    
+
+    if (lnRrad.eq.0._kp) then
+       if (display) write(*,*)'Rrad=1 : solving for rhoReh = rhoEnd'
+    endif
+    
+    epsOneEnd = cnci_epsilon_one(xEnd,alpha)
+    potEnd = cnci_norm_potential(xEnd,alpha)
+    primEnd = cnci_efold_primitive(xEnd,alpha)
+   
+    calF = get_calfconst_rrad(lnRrad,Pstar,epsOneEnd,potEnd)
+
+    cnciData%real1 = alpha
+    cnciData%real2 = calF + primEnd
+
+    maxi = xend*(1._kp-epsilon(1._kp))
+
+!Value of x such that epsilon1=1 below which inflation cannot proceed
+    mini = cnci_x_epsoneunity(alpha)    
+
+    x = zbrent(find_cnci_x_rrad,mini,maxi,tolFind,cnciData)
+    cnci_x_rrad = x
+
+    if (present(bfold)) then
+       bfold = -(cnci_efold_primitive(x,alpha) - primEnd)
+    endif
+
+  end function cnci_x_rrad
+
+  function find_cnci_x_rrad(x,cnciData)   
+    implicit none
+    real(kp) :: find_cnci_x_rrad
+    real(kp), intent(in) :: x
+    type(transfert), optional, intent(inout) :: cnciData
+
+    real(kp) :: primStar,alpha,xend,CalFplusPrimEnd,potStar,epsOneStar
+
+    alpha=cnciData%real1
+    CalFplusPrimEnd = cnciData%real2
+
+    primStar = cnci_efold_primitive(x,alpha)
+    epsOneStar = cnci_epsilon_one(x,alpha)
+    potStar = cnci_norm_potential(x,alpha)
+
+    find_cnci_x_rrad = find_reheat_rrad(PrimStar,calFplusPrimEnd,epsOneStar,potStar)
+
+  end function find_cnci_x_rrad
+
+
+
+!returns x given potential parameters, scalar power, and lnRreh.
+!If present, returns the corresponding bfoldstar
+  function cnci_x_rreh(alpha,xend,lnRreh,bfold)    
+    implicit none
+    real(kp) :: cnci_x_rreh
+    real(kp), intent(in) :: alpha,xEnd,lnRreh
+    real(kp), optional :: bfold
+
+    real(kp), parameter :: tolFind=tolkp
+    real(kp) :: mini,maxi,calF,x
+    real(kp) :: primEnd,epsOneEnd,potEnd
+
+    type(transfert) :: cnciData
+    
+
+    if (lnRreh.eq.0._kp) then
+       if (display) write(*,*)'Rreh=1 : solving for rhoReh = rhoEnd'
+    endif
+    
+    epsOneEnd = cnci_epsilon_one(xEnd,alpha)
+    potEnd = cnci_norm_potential(xEnd,alpha)
+    primEnd = cnci_efold_primitive(xEnd,alpha)
+   
+    calF = get_calfconst_rreh(lnRreh,epsOneEnd,potEnd)
+
+    cnciData%real1 = alpha
+    cnciData%real2 = calF + primEnd
+
+    maxi = xend*(1._kp-epsilon(1._kp))
+
+!Value of x such that epsilon1=1 below which inflation cannot proceed
+    mini = cnci_x_epsoneunity(alpha)    
+
+    x = zbrent(find_cnci_x_rreh,mini,maxi,tolFind,cnciData)
+    cnci_x_rreh = x
+
+    if (present(bfold)) then
+       bfold = -(cnci_efold_primitive(x,alpha) - primEnd)
+    endif
+
+  end function cnci_x_rreh
+
+  function find_cnci_x_rreh(x,cnciData)   
+    implicit none
+    real(kp) :: find_cnci_x_rreh
+    real(kp), intent(in) :: x
+    type(transfert), optional, intent(inout) :: cnciData
+
+    real(kp) :: primStar,alpha,xend,CalFplusPrimEnd,potStar
+
+    alpha=cnciData%real1
+    CalFplusPrimEnd = cnciData%real2
+
+    primStar = cnci_efold_primitive(x,alpha)
+    potStar = cnci_norm_potential(x,alpha)
+
+    find_cnci_x_rreh = find_reheat_rreh(PrimStar,calFplusPrimEnd,potStar)
+
+  end function find_cnci_x_rreh
+
+
 
 
   function cnci_lnrhoreh_max(alpha,xEnd,Pstar) 
