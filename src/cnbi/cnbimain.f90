547b8a4a971ad8b5444e69b88cf7f46b5891ae4d
chris <chris@ringeval.com>
2013-04-25 16:56:56 +0200

    Adding lnR/Rrad for cnbi (+fix main)

--- src/cnbi/cnbimain.f90
+++ src/cnbi/cnbimain.f90
@@ -7,9 +7,15 @@ program cnbimain
   use infinout, only : delete_file, livewrite
   use srreheat, only : log_energy_reheat_ingev
 
+  
+  use cnbisr, only : cnbi_norm_potential, cnbi_x_endinf,cnbi_epsilon_one_min
+  use cnbireheat, only : cnbi_x_rreh, cnbi_x_rrad
+  use srreheat, only : get_lnrrad_rreh, get_lnrreh_rrad, ln_rho_endinf
+  use srreheat, only : get_lnrrad_rhow, get_lnrreh_rhow, ln_rho_reheat
+
   implicit none
 
-  
+
   real(kp) :: Pstar, logErehGeV, Treh
 
   integer :: i,j
@@ -26,10 +32,14 @@ program cnbimain
 
   real(kp) :: eps1A,eps2A,eps3A,nsA,rA,eps1B,eps2B,eps3B,nsB,rB,xstarA,xstarB
 
+  real(kp) :: lnRmin, lnRmax, lnR, lnRhoEnd
+  real(kp) :: lnRradMin, lnRradMax, lnRrad
+  real(kp) :: VendOverVstar, eps1End, xend
+
   nalpha = 300
 
   alphamin=10.**(-5)
-  alphamax=0.2975_kp*0.99_kp
+  alphamax=0.2975_kp*0.1_kp
 
   Pstar = powerAmpScalar
 
@@ -40,72 +50,108 @@ program cnbimain
 
   call delete_file('cnbi_predic.dat')
   call delete_file('cnbi_nsr.dat')
-  
+
   do j=1,nalpha
-  alpha=alphamin*(alphamax/alphamin)**(real(j,kp)/real(nalpha,kp))
+     alpha=alphamin*(alphamax/alphamin)**(real(j-1,kp)/real(nalpha-1,kp))
 
-  lnRhoRehMin = lnRhoNuc
-  lnRhoRehMax = cnbi_lnrhoreh_max(alpha,Pstar)
+     lnRhoRehMin = lnRhoNuc
+     print *,'alpha====',alpha, cnbi_epsilon_one_min(alpha)
+     lnRhoRehMax = cnbi_lnrhoreh_max(alpha,Pstar)
 
-  print *,'alpha=',alpha,'lnRhoRehMin=',lnRhoRehMin, 'lnRhoRehMax= ',lnRhoRehMax
+     print *,'alpha=',alpha,'lnRhoRehMin=',lnRhoRehMin, 'lnRhoRehMax= ',lnRhoRehMax
 
-  do i=1,npts
+     do i=1,npts
 
-       lnRhoReh = lnRhoRehMin + (lnRhoRehMax-lnRhoRehMin)*real(i-1,kp)/real(npts-1,kp)
+        lnRhoReh = lnRhoRehMin + (lnRhoRehMax-lnRhoRehMin)*real(i-1,kp)/real(npts-1,kp)
 
-       xstar = cnbi_x_star(alpha,w,lnRhoReh,Pstar,bfoldstar)
+        xstar = cnbi_x_star(alpha,w,lnRhoReh,Pstar,bfoldstar)
 
+        eps1 = cnbi_epsilon_one(xstar,alpha)
+        eps2 = cnbi_epsilon_two(xstar,alpha)
+        eps3 = cnbi_epsilon_three(xstar,alpha)
 
+        print *,'lnRhoReh',lnRhoReh,' bfoldstar= ',bfoldstar,'xstar=',xstar, &
+             'eps1star=',eps1,'eps2star=',eps2
 
-       eps1 = cnbi_epsilon_one(xstar,alpha)
-       eps2 = cnbi_epsilon_two(xstar,alpha)
-       eps3 = cnbi_epsilon_three(xstar,alpha)
+        logErehGeV = log_energy_reheat_ingev(lnRhoReh)
+        Treh = 10._kp**( logErehGeV -0.25_kp*log10(acos(-1._kp)**2/30._kp) )
 
-       print *,'lnRhoReh',lnRhoReh,' bfoldstar= ',bfoldstar,'xstar=',xstar, &
-         'eps1star=',eps1,'eps2star=',eps2
+        ns = 1._kp - 2._kp*eps1 - eps2
+        r =16._kp*eps1
 
-       logErehGeV = log_energy_reheat_ingev(lnRhoReh)
-       Treh = 10._kp**( logErehGeV -0.25_kp*log10(acos(-1._kp)**2/30._kp) )
+        call livewrite('cnbi_predic.dat',alpha,eps1,eps2,eps3,r,ns,Treh)
 
-       ns = 1._kp - 2._kp*eps1 - eps2
-       r =16._kp*eps1
+        call livewrite('cnbi_nsr.dat',ns,r,abs(bfoldstar),lnRhoReh)
 
-       call livewrite('cnbi_predic.dat',alpha,eps1,eps2,eps3,r,ns,Treh)
+     end do
 
-       call livewrite('cnbi_nsr.dat',ns,r,abs(bfoldstar),lnRhoReh)
-  
-    end do
+  end do
 
- end do
-
-  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   !! Write Data for the summarizing plots !!
-  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
   call delete_file('cnbi_predic_summarized.dat') 
-         nalpha=1000
-         alphamin=10.**(-5)
-         alphamax=0.2975_kp*0.99_kp
-         w=0._kp
-         do j=1,nalpha
-         alpha=alphamin*(alphamax/alphamin)**(real(j,kp)/real(nalpha,kp))
-         lnRhoReh = lnRhoNuc
-         xstarA = cnbi_x_star(alpha,w,lnRhoReh,Pstar,bfoldstar)
-         eps1A = cnbi_epsilon_one(xstarA,alpha)
-         eps2A = cnbi_epsilon_two(xstarA,alpha)
-         eps3A = cnbi_epsilon_three(xstarA,alpha)
-         nsA = 1._kp - 2._kp*eps1A - eps2A
-         rA = 16._kp*eps1A
-         lnRhoReh = cnbi_lnrhoreh_max(alpha,Pstar)
-         xstarB = cnbi_x_star(alpha,w,lnRhoReh,Pstar,bfoldstar)
-         eps1B = cnbi_epsilon_one(xstarB,alpha)
-         eps2B = cnbi_epsilon_two(xstarB,alpha)
-         eps3B = cnbi_epsilon_three(xstarB,alpha)
-         nsB = 1._kp - 2._kp*eps1B - eps2B
-         rB =16._kp*eps1B
-         call livewrite('cnbi_predic_summarized.dat',eps1A,eps2A,eps3A,rA,nsA,eps1B,eps2B,eps3B,rB,nsB)
-         enddo
+  nalpha=1000
+  alphamin=10.**(-5)
+  alphamax=0.2975_kp*0.1_kp
+  w=0._kp
+  do j=1,nalpha
+     alpha=alphamin*(alphamax/alphamin)**(real(j,kp)/real(nalpha,kp))
+     lnRhoReh = lnRhoNuc
+     xstarA = cnbi_x_star(alpha,w,lnRhoReh,Pstar,bfoldstar)
+     eps1A = cnbi_epsilon_one(xstarA,alpha)
+     eps2A = cnbi_epsilon_two(xstarA,alpha)
+     eps3A = cnbi_epsilon_three(xstarA,alpha)
+     nsA = 1._kp - 2._kp*eps1A - eps2A
+     rA = 16._kp*eps1A
+     lnRhoReh = cnbi_lnrhoreh_max(alpha,Pstar)
+     xstarB = cnbi_x_star(alpha,w,lnRhoReh,Pstar,bfoldstar)
+     eps1B = cnbi_epsilon_one(xstarB,alpha)
+     eps2B = cnbi_epsilon_two(xstarB,alpha)
+     eps3B = cnbi_epsilon_three(xstarB,alpha)
+     nsB = 1._kp - 2._kp*eps1B - eps2B
+     rB =16._kp*eps1B
+     call livewrite('cnbi_predic_summarized.dat',eps1A,eps2A,eps3A,rA,nsA,eps1B,eps2B,eps3B,rB,nsB)
+  enddo
 
+  write(*,*)
+  write(*,*)'Testing Rrad/Rreh'
 
+  lnRradmin=-42
+  lnRradmax = 10
+  alpha = 1e-2
+  do i=1,npts
 
-end program cnbimain
+     lnRrad = lnRradMin + (lnRradMax-lnRradMin)*real(i-1,kp)/real(npts-1,kp)
+
+     xstar = cnbi_x_rrad(alpha,lnRrad,Pstar,bfoldstar)
+
+     print *,'lnRrad=',lnRrad,' bfoldstar= ',bfoldstar, 'xstar', xstar
+
+     eps1 = cnbi_epsilon_one(xstar,alpha)
+
+     !consistency test
+     !get lnR from lnRrad and check that it gives the same xstar
+     xend = cnbi_x_endinf(alpha)
+     eps1end =  cnbi_epsilon_one(xend,alpha)
+     VendOverVstar = cnbi_norm_potential(xend,alpha)/cnbi_norm_potential(xstar,alpha)
+
+     lnRhoEnd = ln_rho_endinf(Pstar,eps1,eps1End,VendOverVstar)
+
+     lnR = get_lnrreh_rrad(lnRrad,lnRhoEnd)
+     xstar = cnbi_x_rreh(alpha,lnR,bfoldstar)
+     print *,'lnR',lnR, 'bfoldstar= ',bfoldstar, 'xstar', xstar
+
+     !second consistency check
+     !get rhoreh for chosen w and check that xstar gotten this way is the same
+     w = 0._kp
+     lnRhoReh = ln_rho_reheat(w,Pstar,eps1,eps1End,-bfoldstar,VendOverVstar)
+
+     xstar = cnbi_x_star(alpha,w,lnRhoReh,Pstar,bfoldstar)
+     print *,'lnR', get_lnrreh_rhow(lnRhoReh,w,lnRhoEnd),'lnRrad' &
+          ,get_lnrrad_rhow(lnRhoReh,w,lnRhoEnd),'xstar',xstar
+
+  enddo
+
+end program
--- src/cnbi/cnbireheat.f90
+++ src/cnbi/cnbireheat.f90
@@ -6,6 +6,8 @@ module cnbireheat
   use srreheat, only : get_calfconst, find_reheat, slowroll_validity
   use srreheat, only : display, pi, Nzero, ln_rho_endinf
   use srreheat, only : ln_rho_reheat
+  use srreheat, only : find_reheat_rrad, find_reheat_rreh
+  use srreheat, only : get_calfconst_rrad, get_calfconst_rreh
   use cnbisr, only : cnbi_epsilon_one, cnbi_epsilon_two, cnbi_epsilon_three
   use cnbisr, only : cnbi_norm_potential, cnbi_x_epsoneunity
   use cnbisr, only : cnbi_x_endinf, cnbi_efold_primitive, cnbi_x_trajectory
@@ -14,10 +16,11 @@ module cnbireheat
   private
 
   public cnbi_x_star, cnbi_lnrhoreh_max 
+  public cnbi_x_rrad, cnbi_x_rreh
 
 contains
 
-!returns x=phi/alpha such given potential parameters, scalar power, wreh and
+!returns x=phi/alpha given potential parameters, scalar power, wreh and
 !lnrhoreh. If present, returns the corresponding bfoldstar
   function cnbi_x_star(alpha,w,lnRhoReh,Pstar,bfoldstar)    
     implicit none
@@ -46,10 +49,7 @@ contains
 
     primEnd = cnbi_efold_primitive(xEnd,alpha)
 
-    
-   
     calF = get_calfconst(lnRhoReh,Pstar,w,epsOneEnd,potEnd)
-
 
     cnbiData%real1 = alpha
     cnbiData%real2 = w
@@ -84,9 +84,145 @@ contains
     epsOneStar = cnbi_epsilon_one(x,alpha)
     potStar = cnbi_norm_potential(x,alpha)
 
-    find_cnbi_x_star = find_reheat(primStar,calFplusprimEnd,w,epsOneStar,potStar)
+    find_cnbi_x_star = find_reheat(primStar,calFplusprimEnd,w &
+         ,epsOneStar,potStar)
   
   end function find_cnbi_x_star
+
+!returns x given potential parameters, scalar power, and lnRrad.
+!If present, returns the corresponding bfoldstar
+  function cnbi_x_rrad(alpha,lnRrad,Pstar,bfoldstar)    
+    implicit none
+    real(kp) :: cnbi_x_rrad
+    real(kp), intent(in) :: alpha,lnRrad,Pstar
+    real(kp), intent(out), optional :: bfoldstar
+
+    real(kp), parameter :: tolzbrent=tolkp
+    real(kp) :: mini,maxi,calF,x
+    real(kp) :: primEnd,epsOneEnd,xend,potEnd
+    
+    real(kp), dimension(2) :: xEps1
+
+    type(transfert) :: cnbiData
+    
+
+    if (lnRrad.eq.0._kp) then
+       if (display) write(*,*)'Rrad=1 : solving for rhoReh = rhoEnd'
+    endif
+    
+    xEnd = cnbi_x_endinf(alpha)
+    xEps1 = cnbi_x_epsoneunity(alpha)
+
+    epsOneEnd = cnbi_epsilon_one(xEnd,alpha)
+    potEnd = cnbi_norm_potential(xEnd,alpha)
+
+    primEnd = cnbi_efold_primitive(xEnd,alpha)
+
+    calF = get_calfconst_rrad(lnRrad,Pstar,epsOneEnd,potEnd)
+
+    cnbiData%real1 = alpha
+    cnbiData%real2 = calF + primEnd
+
+    mini = xEnd + epsilon(1._kp)
+    maxi = xEps1(2) + epsilon(1._kp)
+
+
+    x = zbrent(find_cnbi_x_rrad,mini,maxi,tolzbrent,cnbiData)
+    cnbi_x_rrad = x
+
+    if (present(bfoldstar)) then
+       bfoldstar = - (cnbi_efold_primitive(x,alpha) - primEnd)
+    endif
+
+  end function cnbi_x_rrad
+
+  function find_cnbi_x_rrad(x,cnbiData)   
+    implicit none
+    real(kp) :: find_cnbi_x_rrad
+    real(kp), intent(in) :: x
+    type(transfert), optional, intent(inout) :: cnbiData
+
+    real(kp) :: primStar,alpha,w,CalFplusprimEnd,potStar,epsOneStar
+
+    alpha = cnbiData%real1
+    CalFplusprimEnd = cnbiData%real2
+
+    primStar = cnbi_efold_primitive(x,alpha)
+    epsOneStar = cnbi_epsilon_one(x,alpha)
+    potStar = cnbi_norm_potential(x,alpha)
+
+    find_cnbi_x_rrad = find_reheat_rrad(primStar,calFplusprimEnd &
+         ,epsOneStar,potStar)
+  
+  end function find_cnbi_x_rrad
+
+
+
+!returns x given potential parameters, scalar power, and lnRreh.
+!If present, returns the corresponding bfoldstar
+  function cnbi_x_rreh(alpha,lnRreh,bfoldstar)    
+    implicit none
+    real(kp) :: cnbi_x_rreh
+    real(kp), intent(in) :: alpha,lnRreh
+    real(kp), intent(out), optional :: bfoldstar
+
+    real(kp), parameter :: tolzbrent=tolkp
+    real(kp) :: mini,maxi,calF,x
+    real(kp) :: primEnd,epsOneEnd,xend,potEnd
+    
+    real(kp), dimension(2) :: xEps1
+
+    type(transfert) :: cnbiData
+    
+
+    if (lnRreh.eq.0._kp) then
+       if (display) write(*,*)'Rreh=1 : solving for rhoReh = rhoEnd'
+    endif
+    
+    xEnd = cnbi_x_endinf(alpha)
+    xEps1 = cnbi_x_epsoneunity(alpha)
+
+    epsOneEnd = cnbi_epsilon_one(xEnd,alpha)
+    potEnd = cnbi_norm_potential(xEnd,alpha)
+
+    primEnd = cnbi_efold_primitive(xEnd,alpha)
+
+    calF = get_calfconst_rreh(lnRreh,epsOneEnd,potEnd)
+
+    cnbiData%real1 = alpha
+    cnbiData%real2 = calF + primEnd
+
+    mini = xEnd + epsilon(1._kp)
+    maxi = xEps1(2) + epsilon(1._kp)
+
+
+    x = zbrent(find_cnbi_x_rreh,mini,maxi,tolzbrent,cnbiData)
+    cnbi_x_rreh = x
+
+    if (present(bfoldstar)) then
+       bfoldstar = - (cnbi_efold_primitive(x,alpha) - primEnd)
+    endif
+
+  end function cnbi_x_rreh
+
+  function find_cnbi_x_rreh(x,cnbiData)   
+    implicit none
+    real(kp) :: find_cnbi_x_rreh
+    real(kp), intent(in) :: x
+    type(transfert), optional, intent(inout) :: cnbiData
+
+    real(kp) :: primStar,alpha,w,CalFplusprimEnd,potStar
+
+    alpha = cnbiData%real1
+    CalFplusprimEnd = cnbiData%real2
+
+    primStar = cnbi_efold_primitive(x,alpha)   
+    potStar = cnbi_norm_potential(x,alpha)
+
+    find_cnbi_x_rreh = find_reheat_rreh(primStar,calFplusprimEnd &
+         ,potStar)
+  
+  end function find_cnbi_x_rreh
 
 
 
@@ -118,9 +254,11 @@ contains
     potStar = cnbi_norm_potential(x,alpha)
     epsOneStar = cnbi_epsilon_one(x,alpha)
 
-    
-    if (.not.slowroll_validity(epsOneStar)) stop 'cnbi_lnrhoreh_max: slow-roll violated!'
-    
+    if (.not.slowroll_validity(epsOneStar)) then
+       write(*,*)'eps1 = ',epsOneStar
+       stop 'cnbi_lnrhoreh_max: slow-roll violated!'
+    endif
+
     lnRhoEnd = ln_rho_endinf(Pstar,epsOneStar,epsOneEnd,potEnd/potStar)
 
     cnbi_lnrhoreh_max = lnRhoEnd
