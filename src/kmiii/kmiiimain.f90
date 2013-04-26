673f8bc03c43180ec09e264c5855a4b097efb14e
chris <chris@ringeval.com>
2013-04-26 18:47:10 +0200

    Add lnR/Rrad support to kmiii

--- src/kmiii/kmiiimain.f90
+++ src/kmiii/kmiiimain.f90
@@ -8,9 +8,14 @@ program kmiiimain
   use infinout, only : delete_file, livewrite
   use srreheat, only : log_energy_reheat_ingev
 
+  use kmiiisr, only : kmiii_norm_potential, kmiii_x_endinf
+  use kmiiireheat, only : kmiii_x_rreh, kmiii_x_rrad
+  use srreheat, only : get_lnrrad_rreh, get_lnrreh_rrad, ln_rho_endinf
+  use srreheat, only : get_lnrrad_rhow, get_lnrreh_rhow, ln_rho_reheat
+
   implicit none
 
-  
+
   real(kp) :: Pstar, logErehGeV, Treh
 
   integer :: i,j,k
@@ -24,6 +29,10 @@ program kmiiimain
   real(kp), dimension(2) :: vecbuffer
 
   real(kp) ::betamin,betamax,alphamin,alphamax,xendAppr,xend
+
+  real(kp) :: lnRmin, lnRmax, lnR, lnRhoEnd
+  real(kp) :: lnRradMin, lnRradMax, lnRrad
+  real(kp) :: VendOverVstar, eps1End
 
   Pstar = powerAmpScalar
 
@@ -39,9 +48,9 @@ program kmiiimain
   nbetaprior=100
   call delete_file('kmiii_prior.dat')
   do i=0,nbetaprior
-    beta=betamin*(betamax/betamin)**(real(i,kp)/real(nbetaprior,kp))! logarithmic step
-    alpha=kmiii_alphamin(beta)
-    call livewrite('kmiii_prior.dat',beta,alpha) !given beta, writes the minimum value of alpha for which inflation can sto by slow roll violation in the large field domain of the potential
+     beta=betamin*(betamax/betamin)**(real(i,kp)/real(nbetaprior,kp))! logarithmic step
+     alpha=kmiii_alphamin(beta)
+     call livewrite('kmiii_prior.dat',beta,alpha) !given beta, writes the minimum value of alpha for which inflation can sto by slow roll violation in the large field domain of the potential
   end do
   print*,'priors written'
 
@@ -52,18 +61,18 @@ program kmiiimain
 !!!                                   !!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-   nbetaxend=100
-   betamin=10._kp**(10.)
-   betamax=10._kp**(14.)
-   call delete_file('kmiii_xend.dat')
-   do i=0,nbetaxend
+  nbetaxend=100
+  betamin=10._kp**(10.)
+  betamax=10._kp**(14.)
+  call delete_file('kmiii_xend.dat')
+  do i=0,nbetaxend
      beta=betamin*(betamax/betamin)**(real(i,kp)/real(nbetaxend,kp))! logarithmic step
      alpha=kmiii_alphamin(beta)*10._kp
      !alpha=beta/1._kp
      xendAppr=kmiii_x_endinf_appr(alpha,beta)
      xend=kmiii_x_endinf(alpha,beta)
      call livewrite('kmiii_xend.dat',beta,xendAppr,xend)
-   end do
+  end do
   print*,'xend written'
 
 
@@ -77,53 +86,95 @@ program kmiiimain
   call delete_file('kmiii_predic.dat')
   call delete_file('kmiii_nsr.dat')
 
-!  w = 1._kp/3._kp
+  !  w = 1._kp/3._kp
   w=0._kp
 
-   betamin=10._kp**(9.) 
-   betamax=10._kp**(15.)
+  betamin=10._kp**(9.) 
+  betamax=10._kp**(15.)
 
-   do i=0,nbeta
+  do i=0,nbeta
      beta=betamin*(betamax/betamin)**(real(i,kp)/real(nbeta,kp))! logarithmic step
      alphamin=kmiii_alphamin(beta)*1.1_kp
      alphamax=beta/100._kp
      do j=0,nalpha
-     alpha=alphamin*(alphamax/alphamin)**(real(j,kp)/real(nalpha,kp))! logarithmic step
+        alpha=alphamin*(alphamax/alphamin)**(real(j,kp)/real(nalpha,kp))! logarithmic step
 
-    lnRhoRehMin = lnRhoNuc
-    lnRhoRehMax = kmiii_lnrhoreh_max(alpha,beta,Pstar)
-  
+        lnRhoRehMin = lnRhoNuc
+        lnRhoRehMax = kmiii_lnrhoreh_max(alpha,beta,Pstar)
 
-    print *,'alpha=',alpha,'beta=',beta,'lnRhoRehMin=',lnRhoRehMin, 'lnRhoRehMax= ',lnRhoRehMax
-  
 
-  do k=1,npts
+        print *,'alpha=',alpha,'beta=',beta,'lnRhoRehMin=',lnRhoRehMin, 'lnRhoRehMax= ',lnRhoRehMax
 
-       lnRhoReh = lnRhoRehMin + (lnRhoRehMax-lnRhoRehMin)*real(k-1,kp)/real(npts-1,kp)
 
-       xstar = kmiii_x_star(alpha,beta,w,lnRhoReh,Pstar,bfoldstar)
+        do k=1,npts
 
-       eps1 = kmiii_epsilon_one(xstar,alpha,beta)
-       eps2 = kmiii_epsilon_two(xstar,alpha,beta)
-       eps3 = kmiii_epsilon_three(xstar,alpha,beta)
+           lnRhoReh = lnRhoRehMin + (lnRhoRehMax-lnRhoRehMin)*real(k-1,kp)/real(npts-1,kp)
 
-       logErehGeV = log_energy_reheat_ingev(lnRhoReh)
-       Treh = 10._kp**( logErehGeV -0.25_kp*log10(acos(-1._kp)**2/30._kp) )
+           xstar = kmiii_x_star(alpha,beta,w,lnRhoReh,Pstar,bfoldstar)
 
-       ns = 1._kp - 2._kp*eps1 - eps2
-       r =16._kp*eps1
+           eps1 = kmiii_epsilon_one(xstar,alpha,beta)
+           eps2 = kmiii_epsilon_two(xstar,alpha,beta)
+           eps3 = kmiii_epsilon_three(xstar,alpha,beta)
 
-       print *,'lnRhoReh',lnRhoReh,' bfoldstar= ',bfoldstar,'xstar=',xstar,'r=',r,'ns=',ns
+           logErehGeV = log_energy_reheat_ingev(lnRhoReh)
+           Treh = 10._kp**( logErehGeV -0.25_kp*log10(acos(-1._kp)**2/30._kp) )
 
-       call livewrite('kmiii_predic.dat',alpha,beta,eps1,eps2,eps3,r,ns,Treh)
+           ns = 1._kp - 2._kp*eps1 - eps2
+           r =16._kp*eps1
 
-       call livewrite('kmiii_nsr.dat',ns,r,abs(bfoldstar),lnRhoReh)
-  
-    end do
+           print *,'lnRhoReh',lnRhoReh,' bfoldstar= ',bfoldstar,'xstar=',xstar,'r=',r,'ns=',ns
 
-  end do  
+           call livewrite('kmiii_predic.dat',alpha,beta,eps1,eps2,eps3,r,ns,Treh)
 
- end do
+           call livewrite('kmiii_nsr.dat',ns,r,abs(bfoldstar),lnRhoReh)
+
+        end do
+
+     end do
+
+  end do
+
+
+
+  write(*,*)
+  write(*,*)'Testing Rrad/Rreh'
+
+  lnRradmin=-42
+  lnRradmax = 10
+  alpha = kmiii_alphamin(beta)*10._kp
+  beta = 1e10
+  do i=1,npts
+
+     lnRrad = lnRradMin + (lnRradMax-lnRradMin)*real(i-1,kp)/real(npts-1,kp)
+
+     xstar = kmiii_x_rrad(alpha,beta,lnRrad,Pstar,bfoldstar)
+
+     print *,'lnRrad=',lnRrad,' bfoldstar= ',bfoldstar, 'xstar', xstar
+
+     eps1 = kmiii_epsilon_one(xstar,alpha,beta)
+
+     !consistency test
+     !get lnR from lnRrad and check that it gives the same xstar
+     xend = kmiii_x_endinf(alpha,beta)
+     eps1end =  kmiii_epsilon_one(xend,alpha,beta)
+     VendOverVstar = kmiii_norm_potential(xend,alpha,beta)/kmiii_norm_potential(xstar,alpha,beta)
+
+     lnRhoEnd = ln_rho_endinf(Pstar,eps1,eps1End,VendOverVstar)
+
+     lnR = get_lnrreh_rrad(lnRrad,lnRhoEnd)
+     xstar = kmiii_x_rreh(alpha,beta,lnR,bfoldstar)
+     print *,'lnR',lnR, 'bfoldstar= ',bfoldstar, 'xstar', xstar
+
+     !second consistency check
+     !get rhoreh for chosen w and check that xstar gotten this way is the same
+     w = 0._kp
+     lnRhoReh = ln_rho_reheat(w,Pstar,eps1,eps1End,-bfoldstar,VendOverVstar)
+
+     xstar = kmiii_x_star(alpha,beta,w,lnRhoReh,Pstar,bfoldstar)
+     print *,'lnR', get_lnrreh_rhow(lnRhoReh,w,lnRhoEnd),'lnRrad' &
+          ,get_lnrrad_rhow(lnRhoReh,w,lnRhoEnd),'xstar',xstar
+
+  enddo
 
 
 
--- src/kmiii/kmiiireheat.f90
+++ src/kmiii/kmiiireheat.f90
@@ -6,6 +6,8 @@ module kmiiireheat
   use srreheat, only : get_calfconst, find_reheat, slowroll_validity
   use srreheat, only : display, pi, Nzero, ln_rho_endinf
   use srreheat, only : ln_rho_reheat
+  use srreheat, only : find_reheat_rrad, find_reheat_rreh
+  use srreheat, only : get_calfconst_rrad, get_calfconst_rreh
   use kmiiisr, only : kmiii_epsilon_one, kmiii_epsilon_two, kmiii_epsilon_three
   use kmiiisr, only : kmiii_norm_potential
   use kmiiisr, only : kmiii_x_endinf, kmiii_efold_primitive
@@ -13,7 +15,8 @@ module kmiiireheat
 
   private
 
-  public kmiii_x_star, kmiii_lnrhoreh_max 
+  public kmiii_x_star, kmiii_lnrhoreh_max
+  public kmiii_x_rrad, kmiii_x_rreh
 
 contains
 
@@ -81,6 +84,129 @@ contains
   
   end function find_kmiii_x_star
 
+
+
+!returns x given potential parameters, scalar power, and lnRrad.
+!If present, returns the corresponding bfoldstar
+  function kmiii_x_rrad(alpha,beta,lnRrad,Pstar,bfoldstar)    
+    implicit none
+    real(kp) :: kmiii_x_rrad
+    real(kp), intent(in) :: alpha,beta,lnRrad,Pstar
+    real(kp), intent(out), optional :: bfoldstar
+
+    real(kp), parameter :: tolzbrent=tolkp
+    real(kp) :: mini,maxi,calF,x
+    real(kp) :: primEnd,epsOneEnd,xend,potEnd
+
+    type(transfert) :: kmiiiData
+    
+    if (lnRrad.eq.0._kp) then
+       if (display) write(*,*)'Rrad=1 : solving for rhoReh = rhoEnd'
+    endif
+    
+    xEnd = kmiii_x_endinf(alpha,beta)
+    epsOneEnd = kmiii_epsilon_one(xEnd,alpha,beta)
+    potEnd = kmiii_norm_potential(xEnd,alpha,beta)
+    primEnd = kmiii_efold_primitive(xEnd,alpha,beta)
+   
+    calF = get_calfconst_rrad(lnRrad,Pstar,epsOneEnd,potEnd)
+
+    kmiiiData%real1 = alpha    
+    kmiiiData%real2 = beta  
+    kmiiiData%real3 = calF + primEnd
+
+    mini = xEnd
+    maxi = 1._kp/epsilon(1._kp)
+
+    x = zbrent(find_kmiii_x_rrad,mini,maxi,tolzbrent,kmiiiData)
+    kmiii_x_rrad = x
+
+    if (present(bfoldstar)) then
+       bfoldstar = - (kmiii_efold_primitive(x,alpha,beta) - primEnd)
+    endif
+
+  end function kmiii_x_rrad
+
+  function find_kmiii_x_rrad(x,kmiiiData)   
+    implicit none
+    real(kp) :: find_kmiii_x_rrad
+    real(kp), intent(in) :: x
+    type(transfert), optional, intent(inout) :: kmiiiData
+
+    real(kp) :: primStar,alpha,beta,CalFplusprimEnd,potStar,epsOneStar
+
+    alpha=kmiiiData%real1
+    beta=kmiiiData%real2
+    CalFplusprimEnd = kmiiiData%real3
+
+    primStar = kmiii_efold_primitive(x,alpha,beta)
+    epsOneStar = kmiii_epsilon_one(x,alpha,beta)
+    potStar = kmiii_norm_potential(x,alpha,beta)
+
+    find_kmiii_x_rrad = find_reheat_rrad(primStar,calFplusprimEnd,epsOneStar,potStar)
+  
+  end function find_kmiii_x_rrad
+
+
+!returns x given potential parameters, scalar power, and lnRrad.
+!If present, returns the corresponding bfoldstar
+  function kmiii_x_rreh(alpha,beta,lnRreh,bfoldstar)    
+    implicit none
+    real(kp) :: kmiii_x_rreh
+    real(kp), intent(in) :: alpha,beta,lnRreh
+    real(kp), intent(out), optional :: bfoldstar
+
+    real(kp), parameter :: tolzbrent=tolkp
+    real(kp) :: mini,maxi,calF,x
+    real(kp) :: primEnd,epsOneEnd,xend,potEnd
+
+    type(transfert) :: kmiiiData
+    
+    if (lnRreh.eq.0._kp) then
+       if (display) write(*,*)'Rreh=1 : solving for rhoReh = rhoEnd'
+    endif
+    
+    xEnd = kmiii_x_endinf(alpha,beta)
+    epsOneEnd = kmiii_epsilon_one(xEnd,alpha,beta)
+    potEnd = kmiii_norm_potential(xEnd,alpha,beta)
+    primEnd = kmiii_efold_primitive(xEnd,alpha,beta)
+   
+    calF = get_calfconst_rreh(lnRreh,epsOneEnd,potEnd)
+
+    kmiiiData%real1 = alpha    
+    kmiiiData%real2 = beta  
+    kmiiiData%real3 = calF + primEnd
+
+    mini = xEnd
+    maxi = 1._kp/epsilon(1._kp)
+
+    x = zbrent(find_kmiii_x_rreh,mini,maxi,tolzbrent,kmiiiData)
+    kmiii_x_rreh = x
+
+    if (present(bfoldstar)) then
+       bfoldstar = - (kmiii_efold_primitive(x,alpha,beta) - primEnd)
+    endif
+
+  end function kmiii_x_rreh
+
+  function find_kmiii_x_rreh(x,kmiiiData)   
+    implicit none
+    real(kp) :: find_kmiii_x_rreh
+    real(kp), intent(in) :: x
+    type(transfert), optional, intent(inout) :: kmiiiData
+
+    real(kp) :: primStar,alpha,beta,CalFplusprimEnd,potStar
+
+    alpha=kmiiiData%real1
+    beta=kmiiiData%real2
+    CalFplusprimEnd = kmiiiData%real3
+
+    primStar = kmiii_efold_primitive(x,alpha,beta)    
+    potStar = kmiii_norm_potential(x,alpha,beta)
+
+    find_kmiii_x_rreh = find_reheat_rreh(primStar,calFplusprimEnd,potStar)
+  
+  end function find_kmiii_x_rreh
 
 
   function kmiii_lnrhoreh_max(alpha,beta,Pstar) 
