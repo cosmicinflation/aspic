ca86f6c129f29583990ef6507ed66e512cfb4747
chris <chris@ringeval.com>
2013-04-29 19:07:31 +0200

    Add lnR/Rrad support to twi

--- src/twi/twimain.f90
+++ src/twi/twimain.f90
@@ -7,9 +7,14 @@ program twimain
   use infinout, only : delete_file, livewrite
   use srreheat, only : log_energy_reheat_ingev
 
+  use twisr, only : twi_norm_potential
+  use twireheat, only : twi_x_rreh, twi_x_rrad
+  use srreheat, only : get_lnrrad_rreh, get_lnrreh_rrad, ln_rho_endinf
+  use srreheat, only : get_lnrrad_rhow, get_lnrreh_rhow, ln_rho_reheat
+
   implicit none
 
-  
+
   real(kp) :: Pstar, logErehGeV, Treh
 
   integer :: i,j,k
@@ -29,7 +34,9 @@ program twimain
   real(kp) :: lnRhoRehMin, lnRhoRehMax
   real(kp), dimension(2) :: vecbuffer
 
-
+  real(kp) :: lnRmin, lnRmax, lnR, lnRhoEnd
+  real(kp) :: lnRradMin, lnRradMax, lnRrad
+  real(kp) :: VendOverVstar, eps1End
 
   Pstar = powerAmpScalar
 
@@ -37,55 +44,96 @@ program twimain
   call delete_file('twi_nsr.dat')
 
 
-!  w = 1._kp/3._kp
+  !  w = 1._kp/3._kp
   w=0._kp
 
- do j=0,Nphi0 
- phi0=phi0min*(phi0max/phi0min)**(real(j,kp)/Nphi0)
+  do j=0,Nphi0 
+     phi0=phi0min*(phi0max/phi0min)**(real(j,kp)/Nphi0)
 
- do k=0,NxEnd
- xEnd=phi0*yEndmin*(yEndmax/yEndmin)**(real(k,kp)/NxEnd)*(1.+epsilon(1._kp)) !logarithmic step
- xEnd=phi0*yEndmin+(yEndmax-yEndmin)*(real(k,kp)/NxEnd)*phi0*(1.+epsilon(1._kp)) !arithmetic step
- xEnd=phi0*yEndmin*(yEndmax/yEndmin)**(2.**(2.**(real(k,kp)/NxEnd)-1.)-1.)*(1.+epsilon(1._kp)) !ultralogarithmic step
+     do k=0,NxEnd
+        xEnd=phi0*yEndmin*(yEndmax/yEndmin)**(real(k,kp)/NxEnd)*(1.+epsilon(1._kp)) !logarithmic step
+        xEnd=phi0*yEndmin+(yEndmax-yEndmin)*(real(k,kp)/NxEnd)*phi0*(1.+epsilon(1._kp)) !arithmetic step
+        xEnd=phi0*yEndmin*(yEndmax/yEndmin)**(2.**(2.**(real(k,kp)/NxEnd)-1.)-1.)*(1.+epsilon(1._kp)) !ultralogarithmic step
 
 
-  lnRhoRehMin = lnRhoNuc
-  lnRhoRehMax = twi_lnrhoreh_max(phi0,xEnd,Pstar)
+        lnRhoRehMin = lnRhoNuc
+        lnRhoRehMax = twi_lnrhoreh_max(phi0,xEnd,Pstar)
 
-  print *,'phi0=',phi0,'xEnd=',xEnd,'lnRhoRehMin=',lnRhoRehMin, 'lnRhoRehMax= ',lnRhoRehMax
+        print *,'phi0=',phi0,'xEnd=',xEnd,'lnRhoRehMin=',lnRhoRehMin, 'lnRhoRehMax= ',lnRhoRehMax
 
+        do i=1,npts
+
+           lnRhoReh = lnRhoRehMin + (lnRhoRehMax-lnRhoRehMin)*real(i-1,kp)/real(npts-1,kp)
+
+           xstar = twi_x_star(phi0,xEnd,w,lnRhoReh,Pstar,bfoldstar)
+
+           print *,'lnRhoReh',lnRhoReh,' bfoldstar= ',bfoldstar,'xstar=',xstar,'eps1star=',twi_epsilon_one(xstar,phi0)
+
+
+           eps1 = twi_epsilon_one(xstar,phi0)
+           eps2 = twi_epsilon_two(xstar,phi0)
+           eps3 = twi_epsilon_three(xstar,phi0)
+
+
+           logErehGeV = log_energy_reheat_ingev(lnRhoReh)
+
+
+           Treh = 10._kp**( logErehGeV -0.25_kp*log10(acos(-1._kp)**2/30._kp) )
+
+
+           ns = 1._kp - 2._kp*eps1 - eps2
+           r =16._kp*eps1
+
+           call livewrite('twi_predic.dat',phi0,xEnd,eps1,eps2,eps3,r,ns,Treh)
+
+           call livewrite('twi_nsr.dat',ns,r,abs(bfoldstar),lnRhoReh)
+
+        end do
+
+     end do
+
+  end do
+
+
+ write(*,*)
+  write(*,*)'Testing Rrad/Rreh'
+
+  lnRradmin=-42
+  lnRradmax = 10
+  phi0 = 5
+  xend = 90
   do i=1,npts
 
-       lnRhoReh = lnRhoRehMin + (lnRhoRehMax-lnRhoRehMin)*real(i-1,kp)/real(npts-1,kp)
+     lnRrad = lnRradMin + (lnRradMax-lnRradMin)*real(i-1,kp)/real(npts-1,kp)
 
-	xstar = twi_x_star(phi0,xEnd,w,lnRhoReh,Pstar,bfoldstar)
+     xstar = twi_x_rrad(phi0,xend,lnRrad,Pstar,bfoldstar)
 
-       print *,'lnRhoReh',lnRhoReh,' bfoldstar= ',bfoldstar,'xstar=',xstar,'eps1star=',twi_epsilon_one(xstar,phi0)
- 
+     print *,'lnRrad=',lnRrad,' bfoldstar= ',bfoldstar, 'xstar', xstar
 
-       eps1 = twi_epsilon_one(xstar,phi0)
-       eps2 = twi_epsilon_two(xstar,phi0)
-       eps3 = twi_epsilon_three(xstar,phi0)
+     eps1 = twi_epsilon_one(xstar,phi0)
 
+     !consistency test
+     !get lnR from lnRrad and check that it gives the same xstar
+     eps1end =  twi_epsilon_one(xend,phi0)
+     VendOverVstar = twi_norm_potential(xend,phi0)/twi_norm_potential(xstar,phi0)
 
-       logErehGeV = log_energy_reheat_ingev(lnRhoReh)
+     lnRhoEnd = ln_rho_endinf(Pstar,eps1,eps1End,VendOverVstar)
 
+     lnR = get_lnrreh_rrad(lnRrad,lnRhoEnd)
+     xstar = twi_x_rreh(phi0,xend,lnR,bfoldstar)
+     print *,'lnR',lnR, 'bfoldstar= ',bfoldstar, 'xstar', xstar
 
-       Treh = 10._kp**( logErehGeV -0.25_kp*log10(acos(-1._kp)**2/30._kp) )
+     !second consistency check
+     !get rhoreh for chosen w and check that xstar gotten this way is the same
+     w = 0._kp
+     lnRhoReh = ln_rho_reheat(w,Pstar,eps1,eps1End,-bfoldstar,VendOverVstar)
 
+     xstar = twi_x_star(phi0,xend,w,lnRhoReh,Pstar,bfoldstar)
+     print *,'lnR', get_lnrreh_rhow(lnRhoReh,w,lnRhoEnd),'lnRrad' &
+          ,get_lnrrad_rhow(lnRhoReh,w,lnRhoEnd),'xstar',xstar
 
-       ns = 1._kp - 2._kp*eps1 - eps2
-       r =16._kp*eps1
+  enddo
 
-       call livewrite('twi_predic.dat',phi0,xEnd,eps1,eps2,eps3,r,ns,Treh)
-
-       call livewrite('twi_nsr.dat',ns,r,abs(bfoldstar),lnRhoReh)
-  
-    end do
-
- end do
-
-end do
 
 
 end program twimain
--- src/twi/twireheat.f90
+++ src/twi/twireheat.f90
@@ -1,4 +1,4 @@
-!Colemann Weinberg inflation reheating functions in the slow-roll approximations
+!Twisted inflation reheating functions in the slow-roll approximations
 
 module twireheat
   use infprec, only : kp, tolkp, transfert
@@ -6,6 +6,8 @@ module twireheat
   use srreheat, only : get_calfconst, find_reheat, slowroll_validity
   use srreheat, only : display, pi, Nzero, ln_rho_endinf
   use srreheat, only : ln_rho_reheat
+  use srreheat, only : find_reheat_rrad, find_reheat_rreh
+  use srreheat, only : get_calfconst_rrad, get_calfconst_rreh
   use twisr, only : twi_epsilon_one, twi_epsilon_two, twi_epsilon_three
   use twisr, only : twi_norm_potential
   use twisr, only : twi_efold_primitive
@@ -13,7 +15,11 @@ module twireheat
 
   private
 
-  public twi_x_star, twi_lnrhoreh_max 
+!if bigger, <numaccuracy errors
+  real(kp), parameter :: TwiMaxFactor = 100._kp
+
+  public twi_x_star, twi_lnrhoreh_max
+  public twi_x_rrad, twi_x_rreh
 
 contains
 
@@ -31,12 +37,10 @@ contains
 
     type(transfert) :: twiData
     
-
     if (w.eq.1._kp/3._kp) then
        if (display) write(*,*)'w = 1/3 : solving for rhoReh = rhoEnd'
     endif
     
-
     epsOneEnd = twi_epsilon_one(xEnd,phi0)
     potEnd = twi_norm_potential(xEnd,phi0)
 
@@ -49,7 +53,7 @@ contains
     twiData%real3 = calF + primEnd
 
     mini = xEnd
-    maxi=100._kp*phi0 !if bigger, <numaccuracy errors
+    maxi=TwiMaxFactor*phi0 
 
     x = zbrent(find_twi_x_star,mini,maxi,tolzbrent,twiData)
     twi_x_star = x
@@ -79,6 +83,125 @@ contains
     find_twi_x_star = find_reheat(primStar,calFplusprimEnd,w,epsOneStar,potStar)
   
   end function find_twi_x_star
+
+
+!returns x given potential parameters, scalar power, and lnRrad.
+!If present, returns the corresponding bfoldstar
+  function twi_x_rrad(phi0,xEnd,lnRrad,Pstar,bfoldstar)    
+    implicit none
+    real(kp) :: twi_x_rrad
+    real(kp), intent(in) :: phi0,xEnd,lnRrad,Pstar
+    real(kp), intent(out), optional :: bfoldstar
+
+    real(kp), parameter :: tolzbrent=tolkp
+    real(kp) :: mini,maxi,calF,x
+    real(kp) :: primEnd,epsOneEnd,potEnd
+
+    type(transfert) :: twiData
+    
+    if (lnRrad.eq.0._kp) then
+       if (display) write(*,*)'Rrad=1 : solving for rhoReh = rhoEnd'
+    endif
+    
+    epsOneEnd = twi_epsilon_one(xEnd,phi0)
+    potEnd = twi_norm_potential(xEnd,phi0)
+
+    primEnd = twi_efold_primitive(xEnd,phi0)
+   
+    calF = get_calfconst_rrad(lnRrad,Pstar,epsOneEnd,potEnd)
+
+    twiData%real1 = phi0
+    twiData%real2 = calF + primEnd
+
+    mini = xEnd
+    maxi=TwiMaxFactor*phi0 
+
+    x = zbrent(find_twi_x_rrad,mini,maxi,tolzbrent,twiData)
+    twi_x_rrad = x
+
+    if (present(bfoldstar)) then
+       bfoldstar = - (twi_efold_primitive(x,phi0) - primEnd)
+    endif
+
+  end function twi_x_rrad
+
+  function find_twi_x_rrad(x,twiData)   
+    implicit none
+    real(kp) :: find_twi_x_rrad
+    real(kp), intent(in) :: x
+    type(transfert), optional, intent(inout) :: twiData
+
+    real(kp) :: primStar,phi0,CalFplusprimEnd,potStar,epsOneStar
+
+    phi0=twiData%real1
+    CalFplusprimEnd = twiData%real2
+
+    primStar = twi_efold_primitive(x,phi0)
+    epsOneStar = twi_epsilon_one(x,phi0)
+    potStar = twi_norm_potential(x,phi0)
+
+    find_twi_x_rrad = find_reheat_rrad(primStar,calFplusprimEnd,epsOneStar,potStar)
+  
+  end function find_twi_x_rrad
+
+
+!returns x given potential parameters, scalar power, and lnRreh.
+!If present, returns the corresponding bfoldstar
+  function twi_x_rreh(phi0,xEnd,lnRreh,bfoldstar)    
+    implicit none
+    real(kp) :: twi_x_rreh
+    real(kp), intent(in) :: phi0,xEnd,lnRreh
+    real(kp), intent(out), optional :: bfoldstar
+
+    real(kp), parameter :: tolzbrent=tolkp
+    real(kp) :: mini,maxi,calF,x
+    real(kp) :: primEnd,epsOneEnd,potEnd
+
+    type(transfert) :: twiData
+    
+    if (lnRreh.eq.0._kp) then
+       if (display) write(*,*)'Rreh=1 : solving for rhoReh = rhoEnd'
+    endif
+    
+    epsOneEnd = twi_epsilon_one(xEnd,phi0)
+    potEnd = twi_norm_potential(xEnd,phi0)
+
+    primEnd = twi_efold_primitive(xEnd,phi0)
+   
+    calF = get_calfconst_rreh(lnRreh,epsOneEnd,potEnd)
+
+    twiData%real1 = phi0
+    twiData%real2 = calF + primEnd
+
+    mini = xEnd
+    maxi=TwiMaxFactor*phi0 
+
+    x = zbrent(find_twi_x_rreh,mini,maxi,tolzbrent,twiData)
+    twi_x_rreh = x
+
+    if (present(bfoldstar)) then
+       bfoldstar = - (twi_efold_primitive(x,phi0) - primEnd)
+    endif
+
+  end function twi_x_rreh
+
+  function find_twi_x_rreh(x,twiData)   
+    implicit none
+    real(kp) :: find_twi_x_rreh
+    real(kp), intent(in) :: x
+    type(transfert), optional, intent(inout) :: twiData
+
+    real(kp) :: primStar,phi0,CalFplusprimEnd,potStar
+
+    phi0=twiData%real1
+    CalFplusprimEnd = twiData%real2
+
+    primStar = twi_efold_primitive(x,phi0)
+    potStar = twi_norm_potential(x,phi0)
+
+    find_twi_x_rreh = find_reheat_rreh(primStar,calFplusprimEnd,potStar)
+  
+  end function find_twi_x_rreh
 
 
 
