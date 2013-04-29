e81b8c559301961b4f0c6c4e671f790d48a20f61
chris <chris@ringeval.com>
2013-04-29 15:26:47 +0200

    Add lnR/Rrad support to ni

--- src/ni/nimain.f90
+++ src/ni/nimain.f90
@@ -7,8 +7,14 @@ program nimain
   use nireheat, only : ni_x_star
   use infinout, only : delete_file, livewrite
   use srreheat, only : log_energy_reheat_ingev
+
+  use nisr, only : ni_norm_potential, ni_x_endinf
+  use nireheat, only : ni_x_rreh, ni_x_rrad
+  use srreheat, only : get_lnrrad_rreh, get_lnrreh_rrad, ln_rho_endinf
+  use srreheat, only : get_lnrrad_rhow, get_lnrreh_rhow, ln_rho_reheat
+
   implicit none
- 
+
 
   real(kp) :: Pstar, logErehGeV, Treh
 
@@ -27,6 +33,10 @@ program nimain
   real(kp)  ::fmin,fmax,eps1A,eps2A,eps3A,nsA,rA,eps1B,eps2B,eps3B,nsB,rB,xstarA,xstarB
   integer :: nf
 
+  real(kp) :: lnRmin, lnRmax, lnR, lnRhoEnd
+  real(kp) :: lnRradMin, lnRradMax, lnRrad
+  real(kp) :: VendOverVstar, eps1End, xend
+
   fvalues(1)=3.5_kp
   fvalues(2)=4._kp
   fvalues(3)=5._kp
@@ -41,70 +51,110 @@ program nimain
   call delete_file('ni_nsr.dat')
 
   do j=1,size(fvalues)
-   
-  f=fvalues(j)
-  w = 0._kp
- 
-  lnRhoRehMin = lnRhoNuc
-  lnRhoRehMax = ni_lnrhoreh_max(f,Pstar)
 
-  print *,'lnRhoRehMin=',lnRhoRehMin, 'lnRhoRehMax= ',lnRhoRehMax
+     f=fvalues(j)
+     w = 0._kp
 
-  do i=1,npts
+     lnRhoRehMin = lnRhoNuc
+     lnRhoRehMax = ni_lnrhoreh_max(f,Pstar)
 
-     lnRhoReh = lnRhoRehMin + (lnRhoRehMax-lnRhoRehMin)*real(i-1,kp)/real(npts-1,kp)
+     print *,'lnRhoRehMin=',lnRhoRehMin, 'lnRhoRehMax= ',lnRhoRehMax
 
-     xstar = ni_x_star(f,w,lnRhoReh,Pstar,bfoldstar)
+     do i=1,npts
 
-     print *,'lnRhoReh',lnRhoReh,'bfoldstar= ',bfoldstar
+        lnRhoReh = lnRhoRehMin + (lnRhoRehMax-lnRhoRehMin)*real(i-1,kp)/real(npts-1,kp)
 
-     eps1 = ni_epsilon_one(xstar,f)
-     eps2 = ni_epsilon_two(xstar,f)
-     eps3 = ni_epsilon_three(xstar,f)
+        xstar = ni_x_star(f,w,lnRhoReh,Pstar,bfoldstar)
 
-     logErehGeV = log_energy_reheat_ingev(lnRhoReh)
-     Treh =  10._kp**(logErehGeV -0.25_kp*log10(acos(-1._kp)**2/30._kp))
+        print *,'lnRhoReh',lnRhoReh,'bfoldstar= ',bfoldstar
+
+        eps1 = ni_epsilon_one(xstar,f)
+        eps2 = ni_epsilon_two(xstar,f)
+        eps3 = ni_epsilon_three(xstar,f)
+
+        logErehGeV = log_energy_reheat_ingev(lnRhoReh)
+        Treh =  10._kp**(logErehGeV -0.25_kp*log10(acos(-1._kp)**2/30._kp))
 
 
-     ns = 1._kp - 2._kp*eps1 - eps2
-     r =16._kp*eps1
+        ns = 1._kp - 2._kp*eps1 - eps2
+        r =16._kp*eps1
 
-     call livewrite('ni_predic.dat',f,eps1,eps2,eps3,r,ns,Treh)
+        call livewrite('ni_predic.dat',f,eps1,eps2,eps3,r,ns,Treh)
 
-     call livewrite('ni_nsr.dat',ns,r,abs(bfoldstar),lnRhoReh)
-  
+        call livewrite('ni_nsr.dat',ns,r,abs(bfoldstar),lnRhoReh)
+
+     end do
+
   end do
 
-end do
 
-
-  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   !! Write Data for the summarizing plots !!
-  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
   call delete_file('ni_predic_summarized.dat') 
-         nf=1000
-         fmin=10._kp**(0.)
-         fmax=10._kp**(2.)
-         w=0._kp
-         do j=1,nf
-         f=fmin*(fmax/fmin)**(real(j,kp)/real(nf,kp))
-         lnRhoReh = lnRhoNuc
-         xstarA = ni_x_star(f,w,lnRhoReh,Pstar,bfoldstar)
-         eps1A = ni_epsilon_one(xstarA,f)
-         eps2A = ni_epsilon_two(xstarA,f)
-         eps3A = ni_epsilon_three(xstarA,f)
-         nsA = 1._kp - 2._kp*eps1A - eps2A
-         rA = 16._kp*eps1A
-         lnRhoReh = ni_lnrhoreh_max(f,Pstar)
-         xstarB = ni_x_star(f,w,lnRhoReh,Pstar,bfoldstar)
-         eps1B = ni_epsilon_one(xstarB,f)
-         eps2B = ni_epsilon_two(xstarB,f)
-         eps3B = ni_epsilon_three(xstarB,f)
-         nsB = 1._kp - 2._kp*eps1B - eps2B
-         rB =16._kp*eps1B
-         call livewrite('ni_predic_summarized.dat',eps1A,eps2A,eps3A,rA,nsA,eps1B,eps2B,eps3B,rB,nsB)
-         enddo
+  nf=1000
+  fmin=10._kp**(0.)
+  fmax=10._kp**(2.)
+  w=0._kp
+  do j=1,nf
+     f=fmin*(fmax/fmin)**(real(j,kp)/real(nf,kp))
+     lnRhoReh = lnRhoNuc
+     xstarA = ni_x_star(f,w,lnRhoReh,Pstar,bfoldstar)
+     eps1A = ni_epsilon_one(xstarA,f)
+     eps2A = ni_epsilon_two(xstarA,f)
+     eps3A = ni_epsilon_three(xstarA,f)
+     nsA = 1._kp - 2._kp*eps1A - eps2A
+     rA = 16._kp*eps1A
+     lnRhoReh = ni_lnrhoreh_max(f,Pstar)
+     xstarB = ni_x_star(f,w,lnRhoReh,Pstar,bfoldstar)
+     eps1B = ni_epsilon_one(xstarB,f)
+     eps2B = ni_epsilon_two(xstarB,f)
+     eps3B = ni_epsilon_three(xstarB,f)
+     nsB = 1._kp - 2._kp*eps1B - eps2B
+     rB =16._kp*eps1B
+     call livewrite('ni_predic_summarized.dat',eps1A,eps2A,eps3A,rA,nsA,eps1B,eps2B,eps3B,rB,nsB)
+  enddo
+
+  write(*,*)
+  write(*,*)'Testing Rrad/Rreh'
+
+  lnRradmin=-42
+  lnRradmax = 10
+  f = 1.5
+  do i=1,npts
+
+     lnRrad = lnRradMin + (lnRradMax-lnRradMin)*real(i-1,kp)/real(npts-1,kp)
+
+     xstar = ni_x_rrad(f,lnRrad,Pstar,bfoldstar)
+
+     print *,'lnRrad=',lnRrad,' bfoldstar= ',bfoldstar, 'xstar', xstar
+
+     eps1 = ni_epsilon_one(xstar,f)
+
+     !consistency test
+     !get lnR from lnRrad and check that it gives the same xstar
+     xend = ni_x_endinf(f)
+     eps1end =  ni_epsilon_one(xend,f)
+     VendOverVstar = ni_norm_potential(xend,f)/ni_norm_potential(xstar,f)
+
+     lnRhoEnd = ln_rho_endinf(Pstar,eps1,eps1End,VendOverVstar)
+
+     lnR = get_lnrreh_rrad(lnRrad,lnRhoEnd)
+     xstar = ni_x_rreh(f,lnR,bfoldstar)
+     print *,'lnR',lnR, 'bfoldstar= ',bfoldstar, 'xstar', xstar
+
+     !second consistency check
+     !get rhoreh for chosen w and check that xstar gotten this way is the same
+     w = 0._kp
+     lnRhoReh = ln_rho_reheat(w,Pstar,eps1,eps1End,-bfoldstar,VendOverVstar)
+
+     xstar = ni_x_star(f,w,lnRhoReh,Pstar,bfoldstar)
+     print *,'lnR', get_lnrreh_rhow(lnRhoReh,w,lnRhoEnd),'lnRrad' &
+          ,get_lnrrad_rhow(lnRhoReh,w,lnRhoEnd),'xstar',xstar
+
+  enddo
+
 
 
 end program nimain
--- src/ni/nireheat.f90
+++ src/ni/nireheat.f90
@@ -6,6 +6,8 @@ module nireheat
   use srreheat, only : get_calfconst, find_reheat, slowroll_validity
   use srreheat, only : display, pi, Nzero, ln_rho_endinf
   use srreheat, only : ln_rho_reheat
+  use srreheat, only : find_reheat_rrad, find_reheat_rreh
+  use srreheat, only : get_calfconst_rrad, get_calfconst_rreh
   use nisr, only : ni_epsilon_one, ni_epsilon_two
   use nisr, only : ni_norm_potential
   use nisr, only : ni_x_endinf, ni_efold_primitive
@@ -13,7 +15,8 @@ module nireheat
 
   private
 
-  public ni_x_star, ni_lnrhoreh_max 
+  public ni_x_star, ni_lnrhoreh_max
+  public ni_x_rrad, ni_x_rreh
 
 contains
 
@@ -78,6 +81,128 @@ contains
     find_ni_x_star = find_reheat(primStar,calFplusprimEnd,w,epsOneStar,potStar)
 
   end function find_ni_x_star
+
+
+!returns x given potential parameters, scalar power, and lnRrad.
+!If present, returns the corresponding bfoldstar
+  function ni_x_rrad(f,lnRrad,Pstar,bfoldstar)    
+    implicit none
+    real(kp) :: ni_x_rrad
+    real(kp), intent(in) :: f,lnRrad,Pstar
+    real(kp), optional :: bfoldstar
+
+    real(kp), parameter :: tolzbrent=tolkp
+    real(kp) :: mini,maxi,calF,x
+    real(kp) :: primEnd,epsOneEnd,xend,potEnd
+
+    type(transfert) :: niData
+    
+
+    if (lnRrad.eq.0._kp) then
+       if (display) write(*,*)'Rrad=1 : solving for rhoReh = rhoEnd'
+    endif
+    
+    xEnd = ni_x_endinf(f)
+    epsOneEnd = ni_epsilon_one(xEnd,f)
+    potEnd = ni_norm_potential(xEnd,f)
+    primEnd = ni_efold_primitive(xEnd,f)
+   
+    calF = get_calfconst_rrad(lnRrad,Pstar,epsOneEnd,potEnd)
+
+    niData%real1 = f
+    niData%real2 = calF + primEnd
+
+    mini = epsilon(1._kp)
+    maxi = xEnd
+
+    x = zbrent(find_ni_x_rrad,mini,maxi,tolzbrent,niData)
+    ni_x_rrad = x
+
+    if (present(bfoldstar)) then
+       bfoldstar = - (ni_efold_primitive(x,f) - primEnd)
+    endif
+    
+  end function ni_x_rrad
+
+  function find_ni_x_rrad(x,niData)   
+    implicit none
+    real(kp) :: find_ni_x_rrad
+    real(kp), intent(in) :: x
+    type(transfert), optional, intent(inout) :: niData
+
+    real(kp) :: primStar,f,CalFplusprimEnd,potStar,epsOneStar
+
+    f = niData%real1
+    CalFplusprimEnd = niData%real2
+
+    primStar = ni_efold_primitive(x,f)
+    epsOneStar = ni_epsilon_one(x,f)
+    potStar = ni_norm_potential(x,f)
+
+    find_ni_x_rrad = find_reheat_rrad(primStar,calFplusprimEnd,epsOneStar,potStar)
+
+  end function find_ni_x_rrad
+
+
+!returns x given potential parameters, scalar power, and lnRreh.
+!If present, returns the corresponding bfoldstar
+  function ni_x_rreh(f,lnRreh,bfoldstar)    
+    implicit none
+    real(kp) :: ni_x_rreh
+    real(kp), intent(in) :: f,lnRreh
+    real(kp), optional :: bfoldstar
+
+    real(kp), parameter :: tolzbrent=tolkp
+    real(kp) :: mini,maxi,calF,x
+    real(kp) :: primEnd,epsOneEnd,xend,potEnd
+
+    type(transfert) :: niData
+    
+
+    if (lnRreh.eq.0._kp) then
+       if (display) write(*,*)'Rreh=1 : solving for rhoReh = rhoEnd'
+    endif
+    
+    xEnd = ni_x_endinf(f)
+    epsOneEnd = ni_epsilon_one(xEnd,f)
+    potEnd = ni_norm_potential(xEnd,f)
+    primEnd = ni_efold_primitive(xEnd,f)
+   
+    calF = get_calfconst_rreh(lnRreh,epsOneEnd,potEnd)
+
+    niData%real1 = f
+    niData%real2 = calF + primEnd
+
+    mini = epsilon(1._kp)
+    maxi = xEnd
+
+    x = zbrent(find_ni_x_rreh,mini,maxi,tolzbrent,niData)
+    ni_x_rreh = x
+
+    if (present(bfoldstar)) then
+       bfoldstar = - (ni_efold_primitive(x,f) - primEnd)
+    endif
+    
+  end function ni_x_rreh
+
+  function find_ni_x_rreh(x,niData)   
+    implicit none
+    real(kp) :: find_ni_x_rreh
+    real(kp), intent(in) :: x
+    type(transfert), optional, intent(inout) :: niData
+
+    real(kp) :: primStar,f,CalFplusprimEnd,potStar
+
+    f = niData%real1
+    CalFplusprimEnd = niData%real2
+
+    primStar = ni_efold_primitive(x,f)
+    potStar = ni_norm_potential(x,f)
+
+    find_ni_x_rreh = find_reheat_rreh(primStar,calFplusprimEnd,potStar)
+
+  end function find_ni_x_rreh
+
 
 
 
