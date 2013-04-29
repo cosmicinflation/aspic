13eef9c2c2e05f198f44101bf90d261cabc37d93
chris <chris@ringeval.com>
2013-04-29 11:57:50 +0200

    Add lnR/Rrad support to psni

--- src/psni/psnimain.f90
+++ src/psni/psnimain.f90
@@ -7,10 +7,14 @@ program psnimain
   use infinout, only : delete_file, livewrite
   use srreheat, only : log_energy_reheat_ingev
 
+  use psnisr, only : psni_norm_potential, psni_x_endinf
+  use psnireheat, only : psni_x_rreh, psni_x_rrad
+  use srreheat, only : get_lnrrad_rreh, get_lnrreh_rrad, ln_rho_endinf
+  use srreheat, only : get_lnrrad_rhow, get_lnrreh_rhow, ln_rho_reheat
 
   implicit none
 
-  
+
   real(kp) :: Pstar, logErehGeV, Treh
 
   integer :: i,j,k
@@ -24,72 +28,116 @@ program psnimain
   real(kp) :: lnRhoRehMin, lnRhoRehMax
   real(kp), dimension(2) :: vecbuffer
 
+  real(kp) :: lnRmin, lnRmax, lnR, lnRhoEnd
+  real(kp) :: lnRradMin, lnRradMax, lnRrad
+  real(kp) :: VendOverVstar, eps1End, xend
 
   Pstar = powerAmpScalar
 
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-!!        Calculates the reheating predictions           !!
+  !!        Calculates the reheating predictions           !!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   call delete_file('psni_predic.dat')
   call delete_file('psni_nsr.dat')
-  
+
   npts = 20 
   nalpha=200
   w=0._kp
-!  w = 1._kp/3._kp
+  !  w = 1._kp/3._kp
 
-   
-   nf=3
-   allocate(fvalues(1:3))
-   fvalues(3)=0.001_kp
-   fvalues(2)=0.1_kp
-   fvalues(1)=10._kp
-   
-   do j=1,nf
-   f=fvalues(j)
+
+  nf=3
+  allocate(fvalues(1:3))
+  fvalues(3)=0.001_kp
+  fvalues(2)=0.1_kp
+  fvalues(1)=10._kp
+
+  do j=1,nf
+     f=fvalues(j)
 
      alphamax=10._kp**(-1._kp)*f**2
      alphamin=10._kp**(-8._kp)*f**2
 
-    do k=1,nalpha
-       alpha=alphamin*(alphamax/alphamin)**(real(k,kp)/real(nalpha,kp)) !log step
-     
-      lnRhoRehMin = lnRhoNuc
-      lnRhoRehMax = psni_lnrhoreh_max(alpha,f,Pstar)
+     do k=1,nalpha
+        alpha=alphamin*(alphamax/alphamin)**(real(k,kp)/real(nalpha,kp)) !log step
 
-      print *,'alpha=',alpha,'f=',f,'lnRhoRehMin=',lnRhoRehMin, 'lnRhoRehMax= ',lnRhoRehMax
+        lnRhoRehMin = lnRhoNuc
+        lnRhoRehMax = psni_lnrhoreh_max(alpha,f,Pstar)
 
-      do i=1,npts
+        print *,'alpha=',alpha,'f=',f,'lnRhoRehMin=',lnRhoRehMin, 'lnRhoRehMax= ',lnRhoRehMax
 
-       lnRhoReh = lnRhoRehMin + (lnRhoRehMax-lnRhoRehMin)*real(i-1,kp)/real(npts-1,kp)
+        do i=1,npts
 
-       xstar = psni_x_star(alpha,f,w,lnRhoReh,Pstar,bfoldstar)
+           lnRhoReh = lnRhoRehMin + (lnRhoRehMax-lnRhoRehMin)*real(i-1,kp)/real(npts-1,kp)
+
+           xstar = psni_x_star(alpha,f,w,lnRhoReh,Pstar,bfoldstar)
 
 
-       eps1 = psni_epsilon_one(xstar,alpha,f)
-       eps2 = psni_epsilon_two(xstar,alpha,f)
-       eps3 = psni_epsilon_three(xstar,alpha,f)
+           eps1 = psni_epsilon_one(xstar,alpha,f)
+           eps2 = psni_epsilon_two(xstar,alpha,f)
+           eps3 = psni_epsilon_three(xstar,alpha,f)
 
-       print *,'lnRhoReh',lnRhoReh,' bfoldstar= ',bfoldstar,'xstar=',xstar,'eps1star=',eps1
+           print *,'lnRhoReh',lnRhoReh,' bfoldstar= ',bfoldstar,'xstar=',xstar,'eps1star=',eps1
 
-       logErehGeV = log_energy_reheat_ingev(lnRhoReh)
-       Treh = 10._kp**( logErehGeV -0.25_kp*log10(acos(-1._kp)**2/30._kp) )
+           logErehGeV = log_energy_reheat_ingev(lnRhoReh)
+           Treh = 10._kp**( logErehGeV -0.25_kp*log10(acos(-1._kp)**2/30._kp) )
 
-       ns = 1._kp - 2._kp*eps1 - eps2
-       r =16._kp*eps1
+           ns = 1._kp - 2._kp*eps1 - eps2
+           r =16._kp*eps1
 
-       call livewrite('psni_predic.dat',alpha,f,eps1,eps2,eps3,r,ns,Treh)
+           call livewrite('psni_predic.dat',alpha,f,eps1,eps2,eps3,r,ns,Treh)
 
-       call livewrite('psni_nsr.dat',ns,r,abs(bfoldstar),lnRhoReh)
-  
-    end do
+           call livewrite('psni_nsr.dat',ns,r,abs(bfoldstar),lnRhoReh)
+
+        end do
+
+     end do
 
   end do
 
- end do
+
+  write(*,*)
+  write(*,*)'Testing Rrad/Rreh'
+
+  lnRradmin=-42
+  lnRradmax = 10
+  alpha = 1e-4
+  f=0.1
+  do i=1,npts
+
+     lnRrad = lnRradMin + (lnRradMax-lnRradMin)*real(i-1,kp)/real(npts-1,kp)
+
+     xstar = psni_x_rrad(alpha,f,lnRrad,Pstar,bfoldstar)
+
+     print *,'lnRrad=',lnRrad,' bfoldstar= ',bfoldstar, 'xstar', xstar
+
+     eps1 = psni_epsilon_one(xstar,alpha,f)
+
+     !consistency test
+     !get lnR from lnRrad and check that it gives the same xstar
+     xend = psni_x_endinf(alpha,f)
+     eps1end =  psni_epsilon_one(xend,alpha,f)
+     VendOverVstar = psni_norm_potential(xend,alpha,f)/psni_norm_potential(xstar,alpha,f)
+
+     lnRhoEnd = ln_rho_endinf(Pstar,eps1,eps1End,VendOverVstar)
+
+     lnR = get_lnrreh_rrad(lnRrad,lnRhoEnd)
+     xstar = psni_x_rreh(alpha,f,lnR,bfoldstar)
+     print *,'lnR',lnR, 'bfoldstar= ',bfoldstar, 'xstar', xstar
+
+     !second consistency check
+     !get rhoreh for chosen w and check that xstar gotten this way is the same
+     w = 0._kp
+     lnRhoReh = ln_rho_reheat(w,Pstar,eps1,eps1End,-bfoldstar,VendOverVstar)
+
+     xstar = psni_x_star(alpha,f,w,lnRhoReh,Pstar,bfoldstar)
+     print *,'lnR', get_lnrreh_rhow(lnRhoReh,w,lnRhoEnd),'lnRrad' &
+          ,get_lnrrad_rhow(lnRhoReh,w,lnRhoEnd),'xstar',xstar
+
+  enddo
 
 
 
--- src/psni/psnireheat.f90
+++ src/psni/psnireheat.f90
@@ -6,6 +6,8 @@ module psnireheat
   use srreheat, only : get_calfconst, find_reheat, slowroll_validity
   use srreheat, only : display, pi, Nzero, ln_rho_endinf
   use srreheat, only : ln_rho_reheat
+  use srreheat, only : find_reheat_rrad, find_reheat_rreh
+  use srreheat, only : get_calfconst_rrad, get_calfconst_rreh
   use psnisr, only : psni_epsilon_one, psni_epsilon_two, psni_epsilon_three
   use psnisr, only : psni_norm_potential
   use psnisr, only : psni_x_endinf, psni_efold_primitive
@@ -13,7 +15,8 @@ module psnireheat
 
   private
 
-  public psni_x_star, psni_lnrhoreh_max 
+  public psni_x_star, psni_lnrhoreh_max
+  public psni_x_rrad, psni_x_rreh
 
 contains
 
@@ -41,15 +44,12 @@ contains
     potEnd = psni_norm_potential(xEnd,alpha)
     primEnd = psni_efold_primitive(xEnd,alpha,f)
 
-!    print*,'psni_x_star:    xEnd=',xEnd,'  epsOneEnd=',epsOneEnd,'  potEnd=',potEnd,'  primEnd=',primEnd
-   
     calF = get_calfconst(lnRhoReh,Pstar,w,epsOneEnd,potEnd)
 
     psniData%real1 = alpha 
     psniData%real2 = f
-    psniData%real3 = xEnd
-    psniData%real4 = w
-    psniData%real5 = calF + primEnd
+    psniData%real3 = w
+    psniData%real4 = calF + primEnd
 
     mini=epsilon(1._kp)
     maxi = xEnd
@@ -70,13 +70,12 @@ contains
     real(kp), intent(in) :: x
     type(transfert), optional, intent(inout) :: psniData
 
-    real(kp) :: primStar,alpha,f,xEnd,w,CalFplusprimEnd,potStar,epsOneStar
+    real(kp) :: primStar,alpha,f,w,CalFplusprimEnd,potStar,epsOneStar
 
     alpha=psniData%real1
     f=psniData%real2
-    xEnd=psniData%real3
-    w = psniData%real4
-    CalFplusprimEnd = psniData%real5
+    w = psniData%real3
+    CalFplusprimEnd = psniData%real4
 
     primStar = psni_efold_primitive(x,alpha,f)
     epsOneStar = psni_epsilon_one(x,alpha,f)
@@ -85,6 +84,130 @@ contains
     find_psni_x_star = find_reheat(primStar,calFplusprimEnd,w,epsOneStar,potStar)
   
   end function find_psni_x_star
+
+!returns x given potential parameters, scalar power, and lnRrad.
+!If present, returns the corresponding bfoldstar
+  function psni_x_rrad(alpha,f,lnRrad,Pstar,bfoldstar)    
+    implicit none
+    real(kp) :: psni_x_rrad
+    real(kp), intent(in) :: alpha,f,lnRrad,Pstar
+    real(kp), intent(out), optional :: bfoldstar
+
+    real(kp), parameter :: tolzbrent=tolkp
+    real(kp) :: mini,maxi,calF,x
+    real(kp) :: primEnd,epsOneEnd,xend,potEnd
+
+    type(transfert) :: psniData
+    
+    if (lnRrad.eq.0._kp) then
+       if (display) write(*,*)'Rrad=1 : solving for rhoReh = rhoEnd'
+    endif
+
+    xEnd = psni_x_endinf(alpha,f)
+    epsOneEnd = psni_epsilon_one(xEnd,alpha,f)
+    potEnd = psni_norm_potential(xEnd,alpha)
+    primEnd = psni_efold_primitive(xEnd,alpha,f)
+
+    calF = get_calfconst_rrad(lnRrad,Pstar,epsOneEnd,potEnd)
+
+    psniData%real1 = alpha 
+    psniData%real2 = f
+    psniData%real3 = calF + primEnd
+
+    mini=epsilon(1._kp)
+    maxi = xEnd
+
+
+    x = zbrent(find_psni_x_rrad,mini,maxi,tolzbrent,psniData)
+    psni_x_rrad = x
+
+    if (present(bfoldstar)) then
+       bfoldstar = - (psni_efold_primitive(x,alpha,f) - primEnd)
+    endif
+
+  end function psni_x_rrad
+
+  function find_psni_x_rrad(x,psniData)   
+    implicit none
+    real(kp) :: find_psni_x_rrad
+    real(kp), intent(in) :: x
+    type(transfert), optional, intent(inout) :: psniData
+
+    real(kp) :: primStar,alpha,f,CalFplusprimEnd,potStar,epsOneStar
+
+    alpha=psniData%real1
+    f=psniData%real2
+    CalFplusprimEnd = psniData%real3
+
+    primStar = psni_efold_primitive(x,alpha,f)
+    epsOneStar = psni_epsilon_one(x,alpha,f)
+    potStar = psni_norm_potential(x,alpha)
+
+    find_psni_x_rrad = find_reheat_rrad(primStar,calFplusprimEnd,epsOneStar,potStar)
+  
+  end function find_psni_x_rrad
+
+
+!returns x given potential parameters, scalar power, and lnRreh.
+!If present, returns the corresponding bfoldstar
+  function psni_x_rreh(alpha,f,lnRreh,bfoldstar)    
+    implicit none
+    real(kp) :: psni_x_rreh
+    real(kp), intent(in) :: alpha,f,lnRreh
+    real(kp), intent(out), optional :: bfoldstar
+
+    real(kp), parameter :: tolzbrent=tolkp
+    real(kp) :: mini,maxi,calF,x
+    real(kp) :: primEnd,epsOneEnd,xend,potEnd
+
+    type(transfert) :: psniData
+    
+    if (lnRreh.eq.0._kp) then
+       if (display) write(*,*)'Rreh=1 : solving for rhoReh = rhoEnd'
+    endif
+
+    xEnd = psni_x_endinf(alpha,f)
+    epsOneEnd = psni_epsilon_one(xEnd,alpha,f)
+    potEnd = psni_norm_potential(xEnd,alpha)
+    primEnd = psni_efold_primitive(xEnd,alpha,f)
+
+    calF = get_calfconst_rreh(lnRreh,epsOneEnd,potEnd)
+
+    psniData%real1 = alpha 
+    psniData%real2 = f
+    psniData%real3 = calF + primEnd
+
+    mini=epsilon(1._kp)
+    maxi = xEnd
+
+
+    x = zbrent(find_psni_x_rreh,mini,maxi,tolzbrent,psniData)
+    psni_x_rreh = x
+
+    if (present(bfoldstar)) then
+       bfoldstar = - (psni_efold_primitive(x,alpha,f) - primEnd)
+    endif
+
+  end function psni_x_rreh
+
+  function find_psni_x_rreh(x,psniData)   
+    implicit none
+    real(kp) :: find_psni_x_rreh
+    real(kp), intent(in) :: x
+    type(transfert), optional, intent(inout) :: psniData
+
+    real(kp) :: primStar,alpha,f,CalFplusprimEnd,potStar
+
+    alpha=psniData%real1
+    f=psniData%real2
+    CalFplusprimEnd = psniData%real3
+
+    primStar = psni_efold_primitive(x,alpha,f)    
+    potStar = psni_norm_potential(x,alpha)
+
+    find_psni_x_rreh = find_reheat_rreh(primStar,calFplusprimEnd,potStar)
+  
+  end function find_psni_x_rreh
 
 
 
