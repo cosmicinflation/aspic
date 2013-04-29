6b06ae3e454c35dad5f2e3177c2512ff76b0f1c1
chris <chris@ringeval.com>
2013-04-29 19:01:08 +0200

    Add lnR/Rrad support to ti

--- src/ti/timain.f90
+++ src/ti/timain.f90
@@ -7,9 +7,14 @@ program timain
   use infinout, only : delete_file, livewrite
   use srreheat, only : log_energy_reheat_ingev
 
+  use tisr, only : ti_norm_potential, ti_x_endinf
+  use tireheat, only : ti_x_rreh, ti_x_rrad
+  use srreheat, only : get_lnrrad_rreh, get_lnrreh_rrad, ln_rho_endinf
+  use srreheat, only : get_lnrrad_rhow, get_lnrreh_rhow, ln_rho_reheat
+
   implicit none
 
-  
+
   real(kp) :: Pstar, logErehGeV, Treh
 
   integer :: i,j,k
@@ -23,12 +28,16 @@ program timain
 
   real(kp), dimension(:), allocatable ::muvalues
 
+  real(kp) :: lnRmin, lnRmax, lnR, lnRhoEnd
+  real(kp) :: lnRradMin, lnRradMax, lnRrad
+  real(kp) :: VendOverVstar, eps1End, xend
+
   Pstar = powerAmpScalar
 
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-!!        Calculates the reheating predictions           !!
+  !!        Calculates the reheating predictions           !!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
@@ -42,21 +51,21 @@ program timain
   nalpha=100
 
   w=0._kp
-!  w = 1._kp/3._kp
+  !  w = 1._kp/3._kp
 
   call delete_file('ti_predic.dat')
   call delete_file('ti_nsr.dat')
 
   do j=1,size(muvalues)
-    mu=muvalues(j)
+     mu=muvalues(j)
 
-  alphamin=0.5_kp-mu**2/10._kp
-  alphamax=0.5_kp+mu**2/20._kp
+     alphamin=0.5_kp-mu**2/10._kp
+     alphamax=0.5_kp+mu**2/20._kp
 
-    do k=0,nalpha
+     do k=0,nalpha
         alpha=alphamin+(alphamax-alphamin)*(real(k,kp)/real(nalpha,kp))
-    
-     
+
+
         lnRhoRehMin = lnRhoNuc
         lnRhoRehMax = ti_lnrhoreh_max(alpha,mu,Pstar)
 
@@ -83,52 +92,92 @@ program timain
            call livewrite('ti_predic.dat',alpha,(1-2._kp*alpha)/mu**2,mu,eps1,eps2,eps3,r,ns,Treh)
 
            call livewrite('ti_nsr.dat',ns,r,abs(bfoldstar),lnRhoReh)
-  
+
         end do
 
      end do
 
- end do
+  end do
 
- nmu=100
- mumin=10._kp**(-7.)
- mumax=10._kp**(0.)
- alpha=0.5_kp
+  nmu=100
+  mumin=10._kp**(-7.)
+  mumax=10._kp**(0.)
+  alpha=0.5_kp
 
- do j=1,nmu
-    mu=mumin*(mumax/mumin)**(real(j,kp)/real(nmu,kp))
+  do j=1,nmu
+     mu=mumin*(mumax/mumin)**(real(j,kp)/real(nmu,kp))
 
-        lnRhoRehMin = lnRhoNuc
-        lnRhoRehMax = ti_lnrhoreh_max(alpha,mu,Pstar)
+     lnRhoRehMin = lnRhoNuc
+     lnRhoRehMax = ti_lnrhoreh_max(alpha,mu,Pstar)
 
-        print *,'alpha=',alpha,'mu/Mp=',mu,'lnRhoRehMin=',lnRhoRehMin, 'lnRhoRehMax= ',lnRhoRehMax
+     print *,'alpha=',alpha,'mu/Mp=',mu,'lnRhoRehMin=',lnRhoRehMin, 'lnRhoRehMax= ',lnRhoRehMax
 
-        do i=1,npts
+     do i=1,npts
 
-           lnRhoReh = lnRhoRehMin + (lnRhoRehMax-lnRhoRehMin)*real(i-1,kp)/real(npts-1,kp)
+        lnRhoReh = lnRhoRehMin + (lnRhoRehMax-lnRhoRehMin)*real(i-1,kp)/real(npts-1,kp)
 
-           xstar = ti_x_star(alpha,mu,w,lnRhoReh,Pstar,bfoldstar)
+        xstar = ti_x_star(alpha,mu,w,lnRhoReh,Pstar,bfoldstar)
 
-           eps1 = ti_epsilon_one(xstar,alpha,mu)
-           eps2 = ti_epsilon_two(xstar,alpha,mu)
-           eps3 = ti_epsilon_three(xstar,alpha,mu)
+        eps1 = ti_epsilon_one(xstar,alpha,mu)
+        eps2 = ti_epsilon_two(xstar,alpha,mu)
+        eps3 = ti_epsilon_three(xstar,alpha,mu)
 
-           print *,'lnRhoReh',lnRhoReh,' bfoldstar= ',bfoldstar,'xstar=',xstar,'eps1star=',eps1
+        print *,'lnRhoReh',lnRhoReh,' bfoldstar= ',bfoldstar,'xstar=',xstar,'eps1star=',eps1
 
-           logErehGeV = log_energy_reheat_ingev(lnRhoReh)
-           Treh = 10._kp**( logErehGeV -0.25_kp*log10(acos(-1._kp)**2/30._kp) )
+        logErehGeV = log_energy_reheat_ingev(lnRhoReh)
+        Treh = 10._kp**( logErehGeV -0.25_kp*log10(acos(-1._kp)**2/30._kp) )
 
-           ns = 1._kp - 2._kp*eps1 - eps2
-           r =16._kp*eps1
+        ns = 1._kp - 2._kp*eps1 - eps2
+        r =16._kp*eps1
 
-           call livewrite('ti_predic.dat',alpha,0._kp,mu,eps1,eps2,eps3,r,ns,Treh)
+        call livewrite('ti_predic.dat',alpha,0._kp,mu,eps1,eps2,eps3,r,ns,Treh)
 
-           call livewrite('ti_nsr.dat',ns,r,abs(bfoldstar),lnRhoReh)
-  
-        end do
+        call livewrite('ti_nsr.dat',ns,r,abs(bfoldstar),lnRhoReh)
 
-   end do
+     end do
 
+  end do
+
+
+  write(*,*)
+  write(*,*)'Testing Rrad/Rreh'
+
+  lnRradmin=-42
+  lnRradmax = 10
+  alpha = 0.5
+  mu = 0.9
+  do i=1,npts
+
+     lnRrad = lnRradMin + (lnRradMax-lnRradMin)*real(i-1,kp)/real(npts-1,kp)
+
+     xstar = ti_x_rrad(alpha,mu,lnRrad,Pstar,bfoldstar)
+
+     print *,'lnRrad=',lnRrad,' bfoldstar= ',bfoldstar, 'xstar', xstar
+
+     eps1 = ti_epsilon_one(xstar,alpha,mu)
+
+     !consistency test
+     !get lnR from lnRrad and check that it gives the same xstar
+     xend = ti_x_endinf(alpha,mu)
+     eps1end =  ti_epsilon_one(xend,alpha,mu)
+     VendOverVstar = ti_norm_potential(xend,alpha,mu)/ti_norm_potential(xstar,alpha,mu)
+
+     lnRhoEnd = ln_rho_endinf(Pstar,eps1,eps1End,VendOverVstar)
+
+     lnR = get_lnrreh_rrad(lnRrad,lnRhoEnd)
+     xstar = ti_x_rreh(alpha,mu,lnR,bfoldstar)
+     print *,'lnR',lnR, 'bfoldstar= ',bfoldstar, 'xstar', xstar
+
+     !second consistency check
+     !get rhoreh for chosen w and check that xstar gotten this way is the same
+     w = 0._kp
+     lnRhoReh = ln_rho_reheat(w,Pstar,eps1,eps1End,-bfoldstar,VendOverVstar)
+
+     xstar = ti_x_star(alpha,mu,w,lnRhoReh,Pstar,bfoldstar)
+     print *,'lnR', get_lnrreh_rhow(lnRhoReh,w,lnRhoEnd),'lnRrad' &
+          ,get_lnrrad_rhow(lnRhoReh,w,lnRhoEnd),'xstar',xstar
+
+  enddo
 
 
 
--- src/ti/tireheat.f90
+++ src/ti/tireheat.f90
@@ -5,13 +5,16 @@ module tireheat
   use inftools, only : zbrent
   use srreheat, only : get_calfconst, find_reheat, slowroll_validity
   use srreheat, only : display, pi, Nzero, ln_rho_endinf,ln_rho_reheat
+  use srreheat, only : find_reheat_rrad, find_reheat_rreh
+  use srreheat, only : get_calfconst_rrad, get_calfconst_rreh
   use tisr, only : ti_epsilon_one, ti_epsilon_two, ti_epsilon_three
   use tisr, only : ti_norm_potential,ti_efold_primitive,ti_x_endinf,ti_x_potmax
   implicit none
 
   private
 
-  public ti_x_star, ti_lnrhoreh_max,find_ti_x_star
+  public ti_x_star, ti_lnrhoreh_max
+  public ti_x_rrad, ti_x_rreh
 
 contains
 
@@ -29,7 +32,6 @@ contains
 
     type(transfert) :: tiData
     
-
     if (w.eq.1._kp/3._kp) then
        if (display) write(*,*)'w = 1/3 : solving for rhoReh = rhoEnd'
     endif
@@ -57,8 +59,6 @@ contains
        bfold = -(ti_efold_primitive(x,alpha,mu) - primEnd)
     endif
 
-    print*,'xStar=',x,'epsOneStar=',ti_epsilon_one(x,alpha,mu),'primStar=',ti_efold_primitive(x,alpha,mu)
-
   end function ti_x_star
 
   function find_ti_x_star(x,tiData)   
@@ -78,11 +78,135 @@ contains
     epsOneStar = ti_epsilon_one(x,alpha,mu)
     potStar = ti_norm_potential(x,alpha)
 
-
     find_ti_x_star = find_reheat(PrimStar,calFplusPrimEnd,w,epsOneStar,potStar)
 
-
   end function find_ti_x_star
+
+!returns x given potential parameters, scalar power, and lnRrad.
+!If present, returns the corresponding bfoldstar
+  function ti_x_rrad(alpha,mu,lnRrad,Pstar,bfold)    
+    implicit none
+    real(kp) :: ti_x_rrad
+    real(kp), intent(in) :: alpha,mu,lnRrad,Pstar
+    real(kp), optional :: bfold
+
+    real(kp), parameter :: tolFind=tolkp
+    real(kp) :: mini,maxi,calF,x
+    real(kp) :: primEnd,epsOneEnd,xEnd,potEnd
+
+    type(transfert) :: tiData
+    
+    if (lnRrad.eq.0._kp) then
+       if (display) write(*,*)'Rrad=1 : solving for rhoReh = rhoEnd'
+    endif
+
+    xEnd=ti_x_endinf(alpha,mu)
+    
+    epsOneEnd = ti_epsilon_one(xEnd,alpha,mu)
+    potEnd = ti_norm_potential(xEnd,alpha)
+    primEnd = ti_efold_primitive(xEnd,alpha,mu)
+
+    calF = get_calfconst_rrad(lnRrad,Pstar,epsOneEnd,potEnd)
+
+    tiData%real1 = alpha
+    tiData%real2 = mu
+    tiData%real3 = calF + primEnd
+
+    mini=ti_x_potmax(alpha,mu) *(1._kp+epsilon(1._kp)) !potential maximum
+    maxi = xEnd*(1._kp-epsilon(1._kp))
+
+    x = zbrent(find_ti_x_rrad,mini,maxi,tolFind,tiData)
+    ti_x_rrad = x
+
+    if (present(bfold)) then
+       bfold = -(ti_efold_primitive(x,alpha,mu) - primEnd)
+    endif
+
+  end function ti_x_rrad
+
+  function find_ti_x_rrad(x,tiData)   
+    implicit none
+    real(kp) :: find_ti_x_rrad
+    real(kp), intent(in) :: x
+    type(transfert), optional, intent(inout) :: tiData
+
+    real(kp) :: primStar,alpha,mu,CalFplusPrimEnd,potStar,epsOneStar
+
+    alpha=tiData%real1
+    mu=tiData%real2
+    CalFplusPrimEnd = tiData%real3
+
+    primStar = ti_efold_primitive(x,alpha,mu)
+    epsOneStar = ti_epsilon_one(x,alpha,mu)
+    potStar = ti_norm_potential(x,alpha)
+
+    find_ti_x_rrad = find_reheat_rrad(PrimStar,calFplusPrimEnd,epsOneStar,potStar)
+
+  end function find_ti_x_rrad
+
+
+!returns x given potential parameters, scalar power, and lnRreh.
+!If present, returns the corresponding bfoldstar
+  function ti_x_rreh(alpha,mu,lnRreh,bfold)    
+    implicit none
+    real(kp) :: ti_x_rreh
+    real(kp), intent(in) :: alpha,mu,lnRreh
+    real(kp), optional :: bfold
+
+    real(kp), parameter :: tolFind=tolkp
+    real(kp) :: mini,maxi,calF,x
+    real(kp) :: primEnd,epsOneEnd,xEnd,potEnd
+
+    type(transfert) :: tiData
+    
+    if (lnRreh.eq.0._kp) then
+       if (display) write(*,*)'Rreh=1 : solving for rhoReh = rhoEnd'
+    endif
+
+    xEnd=ti_x_endinf(alpha,mu)
+    
+    epsOneEnd = ti_epsilon_one(xEnd,alpha,mu)
+    potEnd = ti_norm_potential(xEnd,alpha)
+    primEnd = ti_efold_primitive(xEnd,alpha,mu)
+
+    calF = get_calfconst_rreh(lnRreh,epsOneEnd,potEnd)
+
+    tiData%real1 = alpha
+    tiData%real2 = mu
+    tiData%real3 = calF + primEnd
+
+    mini=ti_x_potmax(alpha,mu) *(1._kp+epsilon(1._kp)) !potential maximum
+    maxi = xEnd*(1._kp-epsilon(1._kp))
+
+    x = zbrent(find_ti_x_rreh,mini,maxi,tolFind,tiData)
+    ti_x_rreh = x
+
+    if (present(bfold)) then
+       bfold = -(ti_efold_primitive(x,alpha,mu) - primEnd)
+    endif
+
+  end function ti_x_rreh
+
+  function find_ti_x_rreh(x,tiData)   
+    implicit none
+    real(kp) :: find_ti_x_rreh
+    real(kp), intent(in) :: x
+    type(transfert), optional, intent(inout) :: tiData
+
+    real(kp) :: primStar,alpha,mu,CalFplusPrimEnd,potStar
+
+    alpha=tiData%real1
+    mu=tiData%real2
+    CalFplusPrimEnd = tiData%real3
+
+    primStar = ti_efold_primitive(x,alpha,mu)
+    potStar = ti_norm_potential(x,alpha)
+
+    find_ti_x_rreh = find_reheat_rreh(PrimStar,calFplusPrimEnd,potStar)
+
+  end function find_ti_x_rreh
+
+
 
 
   function ti_lnrhoreh_max(alpha,mu,Pstar) 
