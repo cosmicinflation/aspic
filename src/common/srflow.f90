dafcfee118b77bccdd3c829604e9c5523599021a
chris <chris@ringeval.com>
2014-04-19 17:50:09 +0200

    Adding support for runnings tensor in srflow

--- src/common/srflow.f90
+++ src/common/srflow.f90
@@ -6,10 +6,15 @@ module srflow
 
   private
 
+  real(kp), parameter :: pi2 = pi*pi
+
   logical, parameter :: display = .false.
 
   public slowroll_violated
-  public scalar_spectral_index, tensor_to_scalar_ratio, scalar_running
+  public tensor_to_scalar_ratio
+  public scalar_spectral_index, tensor_spectral_index
+  public scalar_running, tensor_running
+  public scalar_running_running, tensor_running_running
   public slowroll_corrections, ln_slowroll_corrections
 
 ! because 1/slowroll_corrections >< inverse_slowroll_corrections
@@ -58,6 +63,42 @@ contains
   end function scalar_spectral_index
 
 
+
+  function tensor_spectral_index(eps)
+    implicit none
+    real(kp) :: tensor_spectral_index
+    real(kp), intent(in), dimension(:) :: eps
+    real(kp) :: nt
+    integer :: neps
+
+    neps = size(eps,1)
+
+    select case (neps)
+              
+    case (1)
+       nt = -2._kp*eps(1)
+
+    case (2)
+       nt = -2._kp*eps(1) &
+            - 2._kp*eps(1)**2 - 2._kp*(1._kp+CConst)*eps(1)*eps(2)
+
+    case(3)
+       nt = -2._kp*eps(1) &
+            - 2._kp*eps(1)**2 - 2._kp*(1._kp+CConst)*eps(1)*eps(2) &
+            - 2._kp*eps(1)**3 - (14._kp+6._kp*CConst-pi2)*eps(1)**2*eps(2) &
+            - (2._kp + 2._kp*CConst*(1._kp+CConst) -pi2/12._kp)*eps(1)*eps(2)*(eps(2)+eps(3))
+            
+    case default
+       stop 'tensor_spectral_index: neps not implemented!'
+    end select
+
+
+    tensor_spectral_index = nt
+
+  end function tensor_spectral_index
+
+
+
   function tensor_to_scalar_ratio(eps)
     implicit none
     real(kp) :: tensor_to_scalar_ratio
@@ -77,9 +118,9 @@ contains
 
     case (3)
        r = 1._kp + CConst*eps(2) &
-            + (CConst - pi**2/2._kp + 4._kp)*eps(1)*eps(2) &
-            + (0.5_kp*CConst**2 - pi**2/8._kp + 1._kp)*eps(2)**2 &
-            + (0.5_kp*CConst**2 - pi**2/24._kp)*eps(2)*eps(3)
+            + (CConst - pi2/2._kp + 4._kp)*eps(1)*eps(2) &
+            + (0.5_kp*CConst**2 - pi2/8._kp + 1._kp)*eps(2)**2 &
+            + (0.5_kp*CConst**2 - pi2/24._kp)*eps(2)*eps(3)
 
     case default
        stop 'tensor_to_scalar_ratio: neps not implemented!'
@@ -130,6 +171,100 @@ contains
   end function scalar_running
 
 
+
+
+  function tensor_running(eps)
+    implicit none
+    real(kp) :: tensor_running
+    real(kp), intent(in), dimension(:) :: eps
+    real(kp) :: alpha
+    integer :: neps
+
+    neps = size(eps,1)
+
+    select case (neps)
+       
+    case (1)
+       alpha = 0._kp
+
+    case (2)
+       alpha = -2._kp*eps(1)*eps(2)
+
+    case (3)
+       alpha = -2._kp*eps(1)*eps(2) &
+            - 6._kp*eps(1)**2*eps(2) - 2._kp*(1._kp+CConst)*eps(1)*eps(2)**2 &
+            - 2._kp*(1._kp+CConst)*eps(1)*eps(2)*eps(3)
+      
+    case default
+       stop 'scalar_running: neps not implemented!'
+    end select
+    
+    tensor_running = alpha
+
+  end function tensor_running
+
+
+
+
+  function scalar_running_running(eps)
+    implicit none
+    real(kp) :: scalar_running_running
+    real(kp), intent(in), dimension(:) :: eps
+    real(kp) :: beta
+    integer :: neps
+
+    neps = size(eps,1)
+
+    select case (neps)
+       
+    case (1,2,3)
+       beta = 0._kp
+
+    case (4)
+       beta = -2._kp*eps(1)*eps(2)**2 - 2._kp*eps(1)*eps(2)*eps(3) &
+            - eps(2)*eps(3)**2 - eps(2)*eps(3)*eps(4)
+      
+    case default
+       stop 'tensor_running_running: neps not implemented!'
+    end select
+    
+    scalar_running_running = beta
+
+
+  end function scalar_running_running
+
+
+
+
+  function tensor_running_running(eps)
+    implicit none
+    real(kp) :: tensor_running_running
+    real(kp), intent(in), dimension(:) :: eps
+    real(kp) :: beta
+    integer :: neps
+
+    neps = size(eps,1)
+
+    select case (neps)
+       
+    case (1,2)
+       beta = 0._kp
+
+    case (3)
+       beta = -2._kp*eps(1)*eps(2)*(eps(2)+eps(3))
+      
+    case default
+       stop 'tensor_running_running: neps not implemented!'
+    end select
+    
+    tensor_running_running = beta
+
+
+  end function tensor_running_running
+
+
+
+
 !this gives the normal slow-roll expansion of P(k*)/[H*^2/(8pi^2 eps1*)]
   function slowroll_corrections(eps)
     implicit none
@@ -156,12 +291,12 @@ contains
     case (3)
        slowroll_corrections = 1._kp - 2._kp*(1._kp+Cconst)*eps(1) &
             - CConst*eps(2) &
-            + (-3._kp + 2._kp*CConst + 2._kp*CConst**2 + pi**2/2._kp) &
+            + (-3._kp + 2._kp*CConst + 2._kp*CConst**2 + pi2/2._kp) &
             *eps(1)**2 &
-            + (-6._kp - Cconst + CConst**2 + 7._kp*pi**2/12._kp) &
+            + (-6._kp - Cconst + CConst**2 + 7._kp*pi2/12._kp) &
             *eps(1)*eps(2) &
-            + (-1._kp + CConst**2/2._kp + pi**2/8._kp)*eps(2)**2 &
-            + (-CConst**2/2._kp + pi**2/24._kp)*eps(2)*eps(3)
+            + (-1._kp + CConst**2/2._kp + pi2/8._kp)*eps(2)**2 &
+            + (-CConst**2/2._kp + pi2/24._kp)*eps(2)*eps(3)
 
     case default
        stop 'inverse_slow_corrections: order not implemented!'
@@ -199,12 +334,12 @@ contains
     case (3)
        inverse_slowroll_corrections = 1._kp + 2._kp*(1._kp+Cconst)*eps(1) &
             + CConst*eps(2) &
-            + (7._kp + 6._kp*CConst + 2._kp*CConst**2 - pi**2/2._kp) &
+            + (7._kp + 6._kp*CConst + 2._kp*CConst**2 - pi2/2._kp) &
             *eps(1)**2 &
-            + (6._kp + 5._kp*Cconst + 3._kp*CConst**2 - 7._kp*pi**2/12._kp) &
+            + (6._kp + 5._kp*Cconst + 3._kp*CConst**2 - 7._kp*pi2/12._kp) &
             *eps(1)*eps(2) &
-            + (1._kp + CConst**2/2._kp - pi**2/8._kp)*eps(2)**2 &
-            + (CConst**2/2._kp - pi**2/24._kp)*eps(2)*eps(3)
+            + (1._kp + CConst**2/2._kp - pi2/8._kp)*eps(2)**2 &
+            + (CConst**2/2._kp - pi2/24._kp)*eps(2)*eps(3)
 
     case default
        stop 'inverse_slow_corrections: order not implemented!'
@@ -241,11 +376,11 @@ contains
     case (3)
        ln_slowroll_corrections = -2._kp*(1._kp+Cconst)*eps(1) &
             - CConst*eps(2) &
-            - (5._kp + 2._kp*CConst - pi**2/2._kp)*eps(1)**2 &
-            - (6._kp + 3._kp*Cconst + CConst**2 - 7._kp*pi**2/12._kp) &
+            - (5._kp + 2._kp*CConst - pi2/2._kp)*eps(1)**2 &
+            - (6._kp + 3._kp*Cconst + CConst**2 - 7._kp*pi2/12._kp) &
             *eps(1)*eps(2) &
-            - (1._kp - pi**2/8._kp)*eps(2)**2 &
-            - (CConst**2/2._kp - pi**2/24._kp)*eps(2)*eps(3)
+            - (1._kp - pi2/8._kp)*eps(2)**2 &
+            - (CConst**2/2._kp - pi2/24._kp)*eps(2)*eps(3)
 
     case default
        stop 'ln_slow_corrections: order not implemented!'
