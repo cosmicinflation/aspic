a3e6a0bc76f44c13053eda8f530e90d94844bdb6
chris <chris@ringeval.com>
2015-08-13 17:27:39 +0200

    Split srflow into two modules

---
+++ src/common/huflow.f90
@@ -0,0 +1,407 @@
+module huflow
+!this is in k* defined by k* eta* = -1 and epsH() stands for the Hubble
+!flow parameters evaluated at eta*
+  use infprec, only : kp, pi, CConst
+
+  implicit none
+
+  private
+
+  real(kp), parameter :: pi2 = pi*pi
+
+  logical, parameter :: display = .false.
+
+  public hf_slowroll_violated
+  public hf_tensor_to_scalar_ratio
+  public hf_scalar_spectral_index, hf_tensor_spectral_index
+  public hf_scalar_running, hf_tensor_running
+  public hf_scalar_running_running, hf_tensor_running_running
+  public hubbleflow_corrections, ln_hubbleflow_corrections
+  public inverse_hubbleflow_corrections, ln_inverse_hubbleflow_corrections
+
+contains
+
+
+  function hf_slowroll_violated(epsH)
+    implicit none
+    logical :: hf_slowroll_violated
+    real(kp), dimension(:), intent(in) :: epsH
+
+    hf_slowroll_violated =  any(abs(epsH).gt.1._kp)       
+
+  end function hf_slowroll_violated
+
+
+
+  function hf_scalar_spectral_index(epsH)
+    implicit none
+    real(kp) :: hf_scalar_spectral_index
+    real(kp), intent(in), dimension(:) :: epsH
+    real(kp) :: nsm1
+    integer :: neps
+
+    neps = size(epsH,1)
+
+    select case (neps)
+              
+    case (2)
+       nsm1 = -2._kp*epsH(1) - epsH(2)
+
+    case (3)
+       nsm1 = -2._kp*epsH(1) - epsH(2)
+       nsm1 = nsm1 - 2._kp*epsH(1)**2 - (3._kp+2._kp*CConst)*epsH(1)*epsH(2) &
+            - CConst*epsH(2)*epsH(3)
+      
+    case default
+       stop 'hf_scalar_spectral_index: neps not implemented!'
+    end select
+
+
+    hf_scalar_spectral_index = nsm1 + 1._kp
+
+  end function hf_scalar_spectral_index
+
+
+
+  function hf_tensor_spectral_index(epsH)
+    implicit none
+    real(kp) :: hf_tensor_spectral_index
+    real(kp), intent(in), dimension(:) :: epsH
+    real(kp) :: nt
+    integer :: neps
+
+    neps = size(epsH,1)
+
+    select case (neps)
+              
+    case (1)
+       nt = -2._kp*epsH(1)
+
+    case (2)
+       nt = -2._kp*epsH(1) &
+            - 2._kp*epsH(1)**2 - 2._kp*(1._kp+CConst)*epsH(1)*epsH(2)
+
+    case(3)
+       nt = -2._kp*epsH(1) &
+            - 2._kp*epsH(1)**2 - 2._kp*(1._kp+CConst)*epsH(1)*epsH(2) &
+            - 2._kp*epsH(1)**3 - (14._kp+6._kp*CConst-pi2)*epsH(1)**2*epsH(2) &
+            - (2._kp + 2._kp*CConst*(1._kp+CConst) -pi2/12._kp)*epsH(1)*epsH(2)*(epsH(2)+epsH(3))
+            
+    case default
+       stop 'hf_tensor_spectral_index: neps not implemented!'
+    end select
+
+
+    hf_tensor_spectral_index = nt
+
+  end function hf_tensor_spectral_index
+
+
+
+  function hf_tensor_to_scalar_ratio(epsH)
+    implicit none
+    real(kp) :: hf_tensor_to_scalar_ratio
+    real(kp), intent(in), dimension(:) :: epsH
+    real(kp) :: r
+    integer :: neps
+
+    neps = size(epsH,1)
+
+    select case (neps)
+       
+    case (1)
+       r = 1._kp
+
+    case (2)
+       r = 1._kp + CConst*epsH(2)
+
+    case (3)
+       r = 1._kp + CConst*epsH(2) &
+            + (CConst - pi2/2._kp + 4._kp)*epsH(1)*epsH(2) &
+            + (0.5_kp*CConst**2 - pi2/8._kp + 1._kp)*epsH(2)**2 &
+            + (0.5_kp*CConst**2 - pi2/24._kp)*epsH(2)*epsH(3)
+
+    case default
+       stop 'hf_tensor_to_scalar_ratio: neps not implemented!'
+    end select
+
+!that may happen if epsH2 > 1, i.e. when slow-roll is violated. If
+!this is the case, we use the zero order formula. If this is not the
+!case, this is nasty and we abort.
+    if (r.lt.0._kp) then
+       if (hf_slowroll_violated(epsH)) then
+          if (display) write(*,*),'hf_tensor_to_scalar_ratio: epsH(:)= ',epsH(:)
+          hf_tensor_to_scalar_ratio = 16._kp*epsH(1)
+       else
+          stop 'hf_tensor_to_scalar_ratio: r < 0!'
+       endif
+    else
+       hf_tensor_to_scalar_ratio = 16._kp*epsH(1)*r
+    end if
+
+
+    
+  end function hf_tensor_to_scalar_ratio
+
+
+  function hf_scalar_running(epsH)
+    implicit none
+    real(kp) :: hf_scalar_running
+    real(kp), intent(in), dimension(:) :: epsH
+    real(kp) :: alpha
+    integer :: neps
+
+    neps = size(epsH,1)
+
+    select case (neps)
+       
+    case (1,2)
+       alpha = 0._kp
+
+    case (3)
+       alpha = -2._kp*epsH(1)*epsH(2) - epsH(2)*epsH(3)
+      
+    case default
+       stop 'hf_scalar_running: neps not implemented!'
+    end select
+    
+    hf_scalar_running = alpha
+
+  end function hf_scalar_running
+
+
+
+
+  function hf_tensor_running(epsH)
+    implicit none
+    real(kp) :: hf_tensor_running
+    real(kp), intent(in), dimension(:) :: epsH
+    real(kp) :: alpha
+    integer :: neps
+
+    neps = size(epsH,1)
+
+    select case (neps)
+       
+    case (1)
+       alpha = 0._kp
+
+    case (2)
+       alpha = -2._kp*epsH(1)*epsH(2)
+
+    case (3)
+       alpha = -2._kp*epsH(1)*epsH(2) &
+            - 6._kp*epsH(1)**2*epsH(2) - 2._kp*(1._kp+CConst)*epsH(1)*epsH(2)**2 &
+            - 2._kp*(1._kp+CConst)*epsH(1)*epsH(2)*epsH(3)
+      
+    case default
+       stop 'hf_scalar_running: neps not implemented!'
+    end select
+    
+    hf_tensor_running = alpha
+
+  end function hf_tensor_running
+
+
+
+
+  function hf_scalar_running_running(epsH)
+    implicit none
+    real(kp) :: hf_scalar_running_running
+    real(kp), intent(in), dimension(:) :: epsH
+    real(kp) :: beta
+    integer :: neps
+
+    neps = size(epsH,1)
+
+    select case (neps)
+       
+    case (1,2,3)
+       beta = 0._kp
+
+    case (4)
+       beta = -2._kp*epsH(1)*epsH(2)**2 - 2._kp*epsH(1)*epsH(2)*epsH(3) &
+            - epsH(2)*epsH(3)**2 - epsH(2)*epsH(3)*epsH(4)
+      
+    case default
+       stop 'hf_tensor_running_running: neps not implemented!'
+    end select
+    
+    hf_scalar_running_running = beta
+
+
+  end function hf_scalar_running_running
+
+
+
+
+  function hf_tensor_running_running(epsH)
+    implicit none
+    real(kp) :: hf_tensor_running_running
+    real(kp), intent(in), dimension(:) :: epsH
+    real(kp) :: beta
+    integer :: neps
+
+    neps = size(epsH,1)
+
+    select case (neps)
+       
+    case (1,2)
+       beta = 0._kp
+
+    case (3)
+       beta = -2._kp*epsH(1)*epsH(2)*(epsH(2)+epsH(3))
+      
+    case default
+       stop 'hf_tensor_running_running: neps not implemented!'
+    end select
+    
+    hf_tensor_running_running = beta
+
+
+  end function hf_tensor_running_running
+
+
+
+
+!this gives the normal slow-roll expansion of P(k*)/[H*^2/(8pi^2 eps1*)]
+  function hubbleflow_corrections(epsH)
+    implicit none
+    real(kp) :: hubbleflow_corrections
+    real(kp), dimension(:), intent(in) :: epsH
+    integer :: neps
+
+    neps = size(epsH,1)
+
+    if (hf_slowroll_violated(epsH)) then
+       write(*,*)'epsH= ',epsH
+       stop 'hubbleflow_corrections not reliable!'
+    endif
+    
+    select case (neps)
+
+    case (1)
+       hubbleflow_corrections = 1._kp
+       
+    case (2)
+       hubbleflow_corrections = 1._kp - 2._kp*(1._kp+Cconst)*epsH(1) &
+            - CConst*epsH(2)
+
+    case (3)
+       hubbleflow_corrections = 1._kp - 2._kp*(1._kp+Cconst)*epsH(1) &
+            - CConst*epsH(2) &
+            + (-3._kp + 2._kp*CConst + 2._kp*CConst**2 + pi2/2._kp) &
+            *epsH(1)**2 &
+            + (-6._kp - Cconst + CConst**2 + 7._kp*pi2/12._kp) &
+            *epsH(1)*epsH(2) &
+            + (-1._kp + CConst**2/2._kp + pi2/8._kp)*epsH(2)**2 &
+            + (-CConst**2/2._kp + pi2/24._kp)*epsH(2)*epsH(3)
+
+    case default
+       stop 'inverse_slow_corrections: order not implemented!'
+
+    end select
+
+  end function hubbleflow_corrections
+
+
+
+!this gives [H*^2/(8pi^2 eps1*)]/P(k*) consitently expanded in slow-roll
+!hence the name "inverse" as opposed to normal slow-roll expansion
+  function inverse_hubbleflow_corrections(epsH)
+    implicit none
+    real(kp) :: inverse_hubbleflow_corrections
+    real(kp), dimension(:), intent(in) :: epsH
+    integer :: neps
+
+    neps = size(epsH,1)
+
+    if (hf_slowroll_violated(epsH)) then
+       write(*,*)'epsH= ',epsH
+       stop 'inverse_hubbleflow_corrections not reliable!'
+    endif
+    
+    select case (neps)
+
+    case (1)
+       inverse_hubbleflow_corrections = 1._kp
+       
+    case (2)
+       inverse_hubbleflow_corrections = 1._kp + 2._kp*(1._kp+Cconst)*epsH(1) &
+            + CConst*epsH(2)
+
+    case (3)
+       inverse_hubbleflow_corrections = 1._kp + 2._kp*(1._kp+Cconst)*epsH(1) &
+            + CConst*epsH(2) &
+            + (7._kp + 6._kp*CConst + 2._kp*CConst**2 - pi2/2._kp) &
+            *epsH(1)**2 &
+            + (6._kp + 5._kp*Cconst + 3._kp*CConst**2 - 7._kp*pi2/12._kp) &
+            *epsH(1)*epsH(2) &
+            + (1._kp + CConst**2/2._kp - pi2/8._kp)*epsH(2)**2 &
+            + (CConst**2/2._kp - pi2/24._kp)*epsH(2)*epsH(3)
+
+    case default
+       stop 'inverse_slow_corrections: order not implemented!'
+
+    end select
+
+  end function inverse_hubbleflow_corrections
+
+
+
+!this gives ln[P(k*)] - ln[H*^2/(8pi^2 eps1*)] consistently expanded
+  function ln_hubbleflow_corrections(epsH)
+    implicit none
+    real(kp) :: ln_hubbleflow_corrections
+    real(kp), dimension(:), intent(in) :: epsH
+    integer :: neps
+    
+    neps = size(epsH,1)
+
+    if (hf_slowroll_violated(epsH)) then
+       write(*,*)'epsH= ',epsH
+       stop 'inverse_hubbleflow_corrections not reliable!'
+    endif
+    
+    select case (neps)
+
+    case (1)
+       ln_hubbleflow_corrections = 0._kp
+       
+    case (2)
+       ln_hubbleflow_corrections = -2._kp*(1._kp+Cconst)*epsH(1) &
+            - CConst*epsH(2)
+
+    case (3)
+       ln_hubbleflow_corrections = -2._kp*(1._kp+Cconst)*epsH(1) &
+            - CConst*epsH(2) &
+            - (5._kp + 2._kp*CConst - pi2/2._kp)*epsH(1)**2 &
+            - (6._kp + 3._kp*Cconst + CConst**2 - 7._kp*pi2/12._kp) &
+            *epsH(1)*epsH(2) &
+            - (1._kp - pi2/8._kp)*epsH(2)**2 &
+            - (CConst**2/2._kp - pi2/24._kp)*epsH(2)*epsH(3)
+
+    case default
+       stop 'ln_slow_corrections: order not implemented!'
+
+    end select
+
+
+  end function ln_hubbleflow_corrections
+
+
+!this gives ln[H*^2/(8pi^2 eps1*)] - ln[P(k*)] consistently expanded
+  function ln_inverse_hubbleflow_corrections(epsH)
+    implicit none
+    real(kp) :: ln_inverse_hubbleflow_corrections
+    real(kp), dimension(:), intent(in) :: epsH
+    integer :: neps
+
+    ln_inverse_hubbleflow_corrections = -ln_hubbleflow_corrections(epsH)
+
+  end function ln_inverse_hubbleflow_corrections
+
+
+
+
+end module huflow
--- src/common/srflow.f90
+++ src/common/srflow.f90
@@ -1,16 +1,16 @@
 module srflow
-!this is in k* defined by k* eta* = -1 and eps() stands for the Hubble
-!flow parameters evaluated at eta*
-  use infprec, only : kp, pi, CConst
+!epsV() stands for the slow-roll parameters derived from the potential
+!at *leading order*, i.e. the ones given by the aspic functions
+  use infprec, only : kp
 
   implicit none
 
   private
 
-  real(kp), parameter :: pi2 = pi*pi
 
   logical, parameter :: display = .false.
 
+  public slowroll_to_hubble
   public slowroll_violated
   public tensor_to_scalar_ratio
   public scalar_spectral_index, tensor_spectral_index
@@ -20,289 +20,160 @@ module srflow
 
 ! because 1/slowroll_corrections >< inverse_slowroll_corrections
 ! it is better to numerically stick to one choice only
-!  public inverse_slowroll_corrections, ln_inverse_slowroll_corrections
+! public inverse_slowroll_corrections, ln_inverse_slowroll_corrections
+
 contains
 
 
-  function slowroll_violated(eps)
+
+  function slowroll_to_hubble(epsV)
+    implicit none
+    real(kp), dimension(:), intent(in) :: epsV
+    real(kp), dimension(size(epsV,1)) :: slowroll_to_hubble
+    real(kp), dimension(size(epsV,1)) :: epsH
+
+!epsH(n) depends on epsV(1:n+1); but this term appears in the power
+!spectra expanded at order n, at order n+1, so it can be neglected
+!(for the power spectra)
+    real(kp), parameter :: epsVnp1 = 0._kp
+
+    integer :: neps
+
+    neps = size(epsV,1)
+
+    select case (neps)
+    
+       case (1,2)
+          epsH = epsV
+
+       case (3)
+          epsH(1) = epsV(1) * (1._kp - epsV(2)/3._kp)
+          epsH(2) = epsV(2) * (1._kp - epsV(2)/6._kp - epsV(3)/3._kp)
+          epsH(3) = epsV(3) * (1._kp - epsV(2)/3._kp - epsVnp1/3._kp)
+
+       case default
+          stop 'slowroll_to_hubble: order not implemented!'
+       end select
+
+       slowroll_to_hubble =  epsH
+
+
+  end function slowroll_to_hubble
+
+
+
+
+  function slowroll_violated(epsV)
+    use huflow, only : hf_slowroll_violated
     implicit none
     logical :: slowroll_violated
-    real(kp), dimension(:), intent(in) :: eps
+    real(kp), dimension(:), intent(in) :: epsV
 
-    slowroll_violated =  any(abs(eps).gt.1._kp)       
+    slowroll_violated =  hf_slowroll_violated(slowroll_to_hubble(epsV))
 
   end function slowroll_violated
 
 
 
-  function scalar_spectral_index(eps)
+
+  function scalar_spectral_index(epsV)
+    use huflow, only : hf_scalar_spectral_index
     implicit none
     real(kp) :: scalar_spectral_index
-    real(kp), intent(in), dimension(:) :: eps
-    real(kp) :: nsm1
-    integer :: neps
-
-    neps = size(eps,1)
-
-    select case (neps)
-              
-    case (2)
-       nsm1 = -2._kp*eps(1) - eps(2)
-
-    case (3)
-       nsm1 = -2._kp*eps(1) - eps(2)
-       nsm1 = nsm1 - 2._kp*eps(1)**2 - (3._kp+2._kp*CConst)*eps(1)*eps(2) &
-            - CConst*eps(2)*eps(3)
-      
-    case default
-       stop 'scalar_spectral_index: neps not implemented!'
-    end select
-
-
-    scalar_spectral_index = nsm1 + 1._kp
+    real(kp), intent(in), dimension(:) :: epsV
+    
+    scalar_spectral_index =  hf_scalar_spectral_index(slowroll_to_hubble(epsV))
 
   end function scalar_spectral_index
 
 
 
-  function tensor_spectral_index(eps)
+
+  function tensor_spectral_index(epsV)
+    use huflow, only : hf_tensor_spectral_index
     implicit none
     real(kp) :: tensor_spectral_index
-    real(kp), intent(in), dimension(:) :: eps
-    real(kp) :: nt
-    integer :: neps
-
-    neps = size(eps,1)
-
-    select case (neps)
-              
-    case (1)
-       nt = -2._kp*eps(1)
-
-    case (2)
-       nt = -2._kp*eps(1) &
-            - 2._kp*eps(1)**2 - 2._kp*(1._kp+CConst)*eps(1)*eps(2)
-
-    case(3)
-       nt = -2._kp*eps(1) &
-            - 2._kp*eps(1)**2 - 2._kp*(1._kp+CConst)*eps(1)*eps(2) &
-            - 2._kp*eps(1)**3 - (14._kp+6._kp*CConst-pi2)*eps(1)**2*eps(2) &
-            - (2._kp + 2._kp*CConst*(1._kp+CConst) -pi2/12._kp)*eps(1)*eps(2)*(eps(2)+eps(3))
-            
-    case default
-       stop 'tensor_spectral_index: neps not implemented!'
-    end select
-
-
-    tensor_spectral_index = nt
+    real(kp), intent(in), dimension(:) :: epsV
+   
+    tensor_spectral_index = hf_tensor_spectral_index(slowroll_to_hubble(epsV))
 
   end function tensor_spectral_index
 
 
 
-  function tensor_to_scalar_ratio(eps)
+
+  function tensor_to_scalar_ratio(epsV)
+    use huflow, only : hf_tensor_to_scalar_ratio
     implicit none
     real(kp) :: tensor_to_scalar_ratio
-    real(kp), intent(in), dimension(:) :: eps
-    real(kp) :: r
-    integer :: neps
-
-    neps = size(eps,1)
-
-    select case (neps)
-       
-    case (1)
-       r = 1._kp
-
-    case (2)
-       r = 1._kp + CConst*eps(2)
-
-    case (3)
-       r = 1._kp + CConst*eps(2) &
-            + (CConst - pi2/2._kp + 4._kp)*eps(1)*eps(2) &
-            + (0.5_kp*CConst**2 - pi2/8._kp + 1._kp)*eps(2)**2 &
-            + (0.5_kp*CConst**2 - pi2/24._kp)*eps(2)*eps(3)
-
-    case default
-       stop 'tensor_to_scalar_ratio: neps not implemented!'
-    end select
-
-!that may happen if eps2 > 1, i.e. when slow-roll is violated. If
-!this is the case, we use the zero order formula. If this is not the
-!case, this is nasty and we abort.
-    if (r.lt.0._kp) then
-       if (slowroll_violated(eps)) then
-          if (display) write(*,*),'tensor_to_scalar_ratio: eps(:)= ',eps(:)
-          tensor_to_scalar_ratio = 16._kp*eps(1)
-       else
-          stop 'tensor_to_scalar_ratio: r < 0!'
-       endif
-    else
-       tensor_to_scalar_ratio = 16._kp*eps(1)*r
-    end if
-
-
+    real(kp), intent(in), dimension(:) :: epsV
+    
+    tensor_to_scalar_ratio = hf_tensor_to_scalar_ratio(slowroll_to_hubble(epsV))
     
   end function tensor_to_scalar_ratio
 
 
-  function scalar_running(eps)
+
+
+  function scalar_running(epsV)
+    use huflow, only : hf_scalar_running
     implicit none
     real(kp) :: scalar_running
-    real(kp), intent(in), dimension(:) :: eps
-    real(kp) :: alpha
-    integer :: neps
+    real(kp), intent(in), dimension(:) :: epsV
 
-    neps = size(eps,1)
-
-    select case (neps)
-       
-    case (1,2)
-       alpha = 0._kp
-
-    case (3)
-       alpha = -2._kp*eps(1)*eps(2) - eps(2)*eps(3)
-      
-    case default
-       stop 'scalar_running: neps not implemented!'
-    end select
-    
-    scalar_running = alpha
+    scalar_running = hf_scalar_running(slowroll_to_hubble(epsV))
 
   end function scalar_running
 
 
 
 
-  function tensor_running(eps)
+  function tensor_running(epsV)
+    use huflow, only : hf_tensor_running
     implicit none
     real(kp) :: tensor_running
-    real(kp), intent(in), dimension(:) :: eps
-    real(kp) :: alpha
-    integer :: neps
-
-    neps = size(eps,1)
-
-    select case (neps)
-       
-    case (1)
-       alpha = 0._kp
-
-    case (2)
-       alpha = -2._kp*eps(1)*eps(2)
-
-    case (3)
-       alpha = -2._kp*eps(1)*eps(2) &
-            - 6._kp*eps(1)**2*eps(2) - 2._kp*(1._kp+CConst)*eps(1)*eps(2)**2 &
-            - 2._kp*(1._kp+CConst)*eps(1)*eps(2)*eps(3)
-      
-    case default
-       stop 'scalar_running: neps not implemented!'
-    end select
+    real(kp), intent(in), dimension(:) :: epsV
     
-    tensor_running = alpha
+    tensor_running = hf_tensor_running(slowroll_to_hubble(epsV))
 
   end function tensor_running
 
 
 
 
-  function scalar_running_running(eps)
+  function scalar_running_running(epsV)
+    use huflow, only : hf_scalar_running_running
     implicit none
     real(kp) :: scalar_running_running
-    real(kp), intent(in), dimension(:) :: eps
-    real(kp) :: beta
-    integer :: neps
-
-    neps = size(eps,1)
-
-    select case (neps)
-       
-    case (1,2,3)
-       beta = 0._kp
-
-    case (4)
-       beta = -2._kp*eps(1)*eps(2)**2 - 2._kp*eps(1)*eps(2)*eps(3) &
-            - eps(2)*eps(3)**2 - eps(2)*eps(3)*eps(4)
-      
-    case default
-       stop 'tensor_running_running: neps not implemented!'
-    end select
-    
-    scalar_running_running = beta
-
+    real(kp), intent(in), dimension(:) :: epsV
+  
+    scalar_running_running = hf_scalar_running_running(slowroll_to_hubble(epsV))
 
   end function scalar_running_running
 
 
 
 
-  function tensor_running_running(eps)
+  function tensor_running_running(epsV)
+    use huflow, only : hf_tensor_running_running
     implicit none
     real(kp) :: tensor_running_running
-    real(kp), intent(in), dimension(:) :: eps
-    real(kp) :: beta
-    integer :: neps
-
-    neps = size(eps,1)
-
-    select case (neps)
-       
-    case (1,2)
-       beta = 0._kp
-
-    case (3)
-       beta = -2._kp*eps(1)*eps(2)*(eps(2)+eps(3))
-      
-    case default
-       stop 'tensor_running_running: neps not implemented!'
-    end select
+    real(kp), intent(in), dimension(:) :: epsV
     
-    tensor_running_running = beta
-
+    tensor_running_running = hf_tensor_running_running(slowroll_to_hubble(epsV))
 
   end function tensor_running_running
 
 
 
-
 !this gives the normal slow-roll expansion of P(k*)/[H*^2/(8pi^2 eps1*)]
-  function slowroll_corrections(eps)
+  function slowroll_corrections(epsV)
+    use huflow, only : hubbleflow_corrections
     implicit none
     real(kp) :: slowroll_corrections
-    real(kp), dimension(:), intent(in) :: eps
-    integer :: neps
-
-    neps = size(eps,1)
-
-    if (slowroll_violated(eps)) then
-       write(*,*)'eps= ',eps
-       stop 'slowroll_corrections not reliable!'
-    endif
-    
-    select case (neps)
-
-    case (1)
-       slowroll_corrections = 1._kp
-       
-    case (2)
-       slowroll_corrections = 1._kp - 2._kp*(1._kp+Cconst)*eps(1) &
-            - CConst*eps(2)
-
-    case (3)
-       slowroll_corrections = 1._kp - 2._kp*(1._kp+Cconst)*eps(1) &
-            - CConst*eps(2) &
-            + (-3._kp + 2._kp*CConst + 2._kp*CConst**2 + pi2/2._kp) &
-            *eps(1)**2 &
-            + (-6._kp - Cconst + CConst**2 + 7._kp*pi2/12._kp) &
-            *eps(1)*eps(2) &
-            + (-1._kp + CConst**2/2._kp + pi2/8._kp)*eps(2)**2 &
-            + (-CConst**2/2._kp + pi2/24._kp)*eps(2)*eps(3)
-
-    case default
-       stop 'inverse_slow_corrections: order not implemented!'
-
-    end select
+    real(kp), dimension(:), intent(in) :: epsV
+   
+    slowroll_corrections = hubbleflow_corrections(slowroll_to_hubble(epsV))
 
   end function slowroll_corrections
 
@@ -310,99 +181,40 @@ contains
 
 !this gives [H*^2/(8pi^2 eps1*)]/P(k*) consitently expanded in slow-roll
 !hence the name "inverse" as opposed to normal slow-roll expansion
-  function inverse_slowroll_corrections(eps)
+  function inverse_slowroll_corrections(epsV)
+    use huflow, only : inverse_hubbleflow_corrections
     implicit none
     real(kp) :: inverse_slowroll_corrections
-    real(kp), dimension(:), intent(in) :: eps
-    integer :: neps
-
-    neps = size(eps,1)
-
-    if (slowroll_violated(eps)) then
-       write(*,*)'eps= ',eps
-       stop 'inverse_slowroll_corrections not reliable!'
-    endif
+    real(kp), dimension(:), intent(in) :: epsV
     
-    select case (neps)
-
-    case (1)
-       inverse_slowroll_corrections = 1._kp
-       
-    case (2)
-       inverse_slowroll_corrections = 1._kp + 2._kp*(1._kp+Cconst)*eps(1) &
-            + CConst*eps(2)
-
-    case (3)
-       inverse_slowroll_corrections = 1._kp + 2._kp*(1._kp+Cconst)*eps(1) &
-            + CConst*eps(2) &
-            + (7._kp + 6._kp*CConst + 2._kp*CConst**2 - pi2/2._kp) &
-            *eps(1)**2 &
-            + (6._kp + 5._kp*Cconst + 3._kp*CConst**2 - 7._kp*pi2/12._kp) &
-            *eps(1)*eps(2) &
-            + (1._kp + CConst**2/2._kp - pi2/8._kp)*eps(2)**2 &
-            + (CConst**2/2._kp - pi2/24._kp)*eps(2)*eps(3)
-
-    case default
-       stop 'inverse_slow_corrections: order not implemented!'
-
-    end select
+    inverse_slowroll_corrections = inverse_hubbleflow_corrections(slowroll_to_hubble(epsV))
 
   end function inverse_slowroll_corrections
 
 
 
 !this gives ln[P(k*)] - ln[H*^2/(8pi^2 eps1*)] consistently expanded
-  function ln_slowroll_corrections(eps)
+  function ln_slowroll_corrections(epsV)
+    use huflow, only : ln_hubbleflow_corrections
     implicit none
     real(kp) :: ln_slowroll_corrections
-    real(kp), dimension(:), intent(in) :: eps
-    integer :: neps
+    real(kp), dimension(:), intent(in) :: epsV
     
-    neps = size(eps,1)
-
-    if (slowroll_violated(eps)) then
-       write(*,*)'eps= ',eps
-       stop 'inverse_slowroll_corrections not reliable!'
-    endif
-    
-    select case (neps)
-
-    case (1)
-       ln_slowroll_corrections = 0._kp
-       
-    case (2)
-       ln_slowroll_corrections = -2._kp*(1._kp+Cconst)*eps(1) &
-            - CConst*eps(2)
-
-    case (3)
-       ln_slowroll_corrections = -2._kp*(1._kp+Cconst)*eps(1) &
-            - CConst*eps(2) &
-            - (5._kp + 2._kp*CConst - pi2/2._kp)*eps(1)**2 &
-            - (6._kp + 3._kp*Cconst + CConst**2 - 7._kp*pi2/12._kp) &
-            *eps(1)*eps(2) &
-            - (1._kp - pi2/8._kp)*eps(2)**2 &
-            - (CConst**2/2._kp - pi2/24._kp)*eps(2)*eps(3)
-
-    case default
-       stop 'ln_slow_corrections: order not implemented!'
-
-    end select
-
+    ln_slowroll_corrections = ln_hubbleflow_corrections(slowroll_to_hubble(epsV))
 
   end function ln_slowroll_corrections
 
 
 !this gives ln[H*^2/(8pi^2 eps1*)] - ln[P(k*)] consistently expanded
-  function ln_inverse_slowroll_corrections(eps)
+  function ln_inverse_slowroll_corrections(epsV)
+    use huflow, only : ln_inverse_hubbleflow_corrections
     implicit none
     real(kp) :: ln_inverse_slowroll_corrections
-    real(kp), dimension(:), intent(in) :: eps
-    integer :: neps
+    real(kp), dimension(:), intent(in) :: epsV
 
-    ln_inverse_slowroll_corrections = -ln_slowroll_corrections(eps)
+    ln_inverse_slowroll_corrections = ln_inverse_hubbleflow_corrections(slowroll_to_hubble(epsV))
 
   end function ln_inverse_slowroll_corrections
-
 
 
 
