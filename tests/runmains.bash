2134a31ffe2ae11f1d0d60de76365cffa652e7c7
chris <chris@ringeval.com>
2016-11-02 17:04:09 +0100

    Running testsuite parallel, requires nproc

---
+++ tests/runmains.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mainlist=`ls $1/src/*/*main`
+njobs=`nproc`
+count=0
+for x in $mainlist; do
+    ($x) &
+    count=$[$count + 1]
+    [[ $((count%njobs)) -eq 0 ]] && wait
+done
+
+exit 0
